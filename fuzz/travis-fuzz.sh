73fca023aee03729ba11f6f5641e61cccfce410d
Matt Corallo <git@bluematt.me>
2018-03-20 12:28:18 -0400

    Set travis to do 1,000,000 hfuzz steps

---
+++ fuzz/travis-fuzz.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cargo install honggfuzz
+set +e
+for TARGET in fuzz_targets/*; do
+    FILENAME=$(basename $TARGET)
+	FILE="${FILENAME%.*}"
+	HFUZZ_BUILD_ARGS="--features honggfuzz_fuzz" HFUZZ_RUN_ARGS="-N1000000 --exit_upon_crash -v" cargo hfuzz run $FILE
+	if [ -f hfuzz_workspace/$FILE/HONGGFUZZ.REPORT.TXT ]; then 
+		cat hfuzz_workspace/$FILE/HONGGFUZZ.REPORT.TXT
+		for CASE in hfuzz_workspace/$FILE/SIG*; do
+			cat $CASE | xxd -p
+		done
+		exit 1
+	fi
+done
