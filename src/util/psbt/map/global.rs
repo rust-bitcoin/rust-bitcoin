e5b59120c5e352d7ad0181916a68aadd72a87e7f
Carl Dong <accounts@carldong.me>
2018-09-09 03:34:29 -0700

    Add copyright notice to PSBT-related files

--- src/util/psbt/map/global.rs
+++ src/util/psbt/map/global.rs
@@ -1,3 +1,17 @@
+// Rust Bitcoin Library
+// Written by
+//   The Rust Bitcoin developers
+//
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to
+// the public domain worldwide. This software is distributed without
+// any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication
+// along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//
+
 use std::collections::HashMap;
 use std::io::Cursor;
 
