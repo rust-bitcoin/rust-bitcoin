d78e58e9701071c189a47036ff5559f1b2e03a17
Antoine Riard <ariard@student.42.fr>
2018-12-19 20:56:39 -0500

    Fix comment on transaction version

--- src/blockdata/transaction.rs
+++ src/blockdata/transaction.rs
@@ -241,7 +241,7 @@ impl Default for TxOut {
 /// for 0-input transactions, which results in unambiguously parseable transactions.
 #[derive(Clone, PartialEq, Eq, Debug, Hash)]
 pub struct Transaction {
-    /// The protocol version, should always be 1.
+    /// The protocol version, is currently expected to be 1 or 2 (BIP 68).
     pub version: u32,
     /// Block number before which this transaction is valid, or 0 for
     /// valid immediately.
