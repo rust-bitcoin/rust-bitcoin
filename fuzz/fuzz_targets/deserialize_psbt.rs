75ea1a68aba0d6aad6910c6b24015c3c0699cf66
Riccardo Casatta <riccardo@casatta.it>
2019-08-09 10:28:47 +0200

    psbt fuzz roundtrip on our ser

--- fuzz/fuzz_targets/deserialize_psbt.rs
+++ fuzz/fuzz_targets/deserialize_psbt.rs
@@ -6,7 +6,9 @@ fn do_test(data: &[u8]) {
         Err(_) => {},
         Ok(psbt) => {
             let ser = bitcoin::consensus::encode::serialize(&psbt);
-            assert_eq!(&ser[..], data);
+            let deser: bitcoin::util::psbt::PartiallySignedTransaction  = bitcoin::consensus::encode::deserialize(&ser).unwrap();
+            // Since the fuzz data could order psbt fields differently, we compare to our deser/ser instead of data
+            assert_eq!(ser, bitcoin::consensus::encode::serialize(&deser));
         }
     }
 }
