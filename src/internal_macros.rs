521a2f34519083506411801426fce7ee4efd4608
Elichai Turkel <elichai.turkel@gmail.com>
2019-12-02 13:00:09 +0200

    Remove derivable and unused functions from internal_macros

--- src/internal_macros.rs
+++ src/internal_macros.rs
@@ -45,41 +45,6 @@ macro_rules! impl_consensus_encoding {
 
 macro_rules! impl_array_newtype {
     ($thing:ident, $ty:ty, $len:expr) => {
-        impl $thing {
-            #[inline]
-            /// Converts the object to a raw pointer
-            pub fn as_ptr(&self) -> *const $ty {
-                let &$thing(ref dat) = self;
-                dat.as_ptr()
-            }
-
-            #[inline]
-            /// Converts the object to a mutable raw pointer
-            pub fn as_mut_ptr(&mut self) -> *mut $ty {
-                let &mut $thing(ref mut dat) = self;
-                dat.as_mut_ptr()
-            }
-
-            #[inline]
-            /// Returns the length of the object as an array
-            pub fn len(&self) -> usize { $len }
-
-            #[inline]
-            /// Returns whether the object, as an array, is empty. Always false.
-            pub fn is_empty(&self) -> bool { false }
-
-            #[inline]
-            /// Returns the underlying bytes.
-            pub fn as_bytes(&self) -> &[$ty; $len] { &self.0 }
-
-            #[inline]
-            /// Returns the underlying bytes.
-            pub fn to_bytes(&self) -> [$ty; $len] { self.0.clone() }
-
-            #[inline]
-            /// Returns the underlying bytes.
-            pub fn into_bytes(self) -> [$ty; $len] { self.0 }
-        }
 
         impl<'a> From<&'a [$ty]> for $thing {
             fn from(data: &'a [$ty]) -> $thing {
@@ -90,75 +55,7 @@ macro_rules! impl_array_newtype {
             }
         }
 
-        impl ::std::ops::Index<usize> for $thing {
-            type Output = $ty;
-
-            #[inline]
-            fn index(&self, index: usize) -> &$ty {
-                let &$thing(ref dat) = self;
-                &dat[index]
-            }
-        }
-
         impl_index_newtype!($thing, $ty);
-
-        impl PartialEq for $thing {
-            #[inline]
-            fn eq(&self, other: &$thing) -> bool {
-                &self[..] == &other[..]
-            }
-        }
-
-        impl Eq for $thing {}
-
-        impl PartialOrd for $thing {
-            #[inline]
-            fn partial_cmp(&self, other: &$thing) -> Option<::std::cmp::Ordering> {
-                Some(self.cmp(&other))
-            }
-        }
-
-        impl Ord for $thing {
-            #[inline]
-            fn cmp(&self, other: &$thing) -> ::std::cmp::Ordering {
-                // manually implement comparison to get little-endian ordering
-                // (we need this for our numeric types; non-numeric ones shouldn't
-                // be ordered anyway except to put them in BTrees or whatever, and
-                // they don't care how we order as long as we're consistent).
-                for i in 0..$len {
-                    if self[$len - 1 - i] < other[$len - 1 - i] { return ::std::cmp::Ordering::Less; }
-                    if self[$len - 1 - i] > other[$len - 1 - i] { return ::std::cmp::Ordering::Greater; }
-                }
-                ::std::cmp::Ordering::Equal
-            }
-        }
-
-        #[cfg_attr(feature = "clippy", allow(expl_impl_clone_on_copy))] // we don't define the `struct`, we have to explicitly impl
-        impl Clone for $thing {
-            #[inline]
-            fn clone(&self) -> $thing {
-                $thing::from(&self[..])
-            }
-        }
-
-        impl Copy for $thing {}
-
-        impl ::std::hash::Hash for $thing {
-            #[inline]
-            fn hash<H>(&self, state: &mut H)
-                where H: ::std::hash::Hasher
-            {
-                (&self[..]).hash(state);
-            }
-
-            fn hash_slice<H>(data: &[$thing], state: &mut H)
-                where H: ::std::hash::Hasher
-            {
-                for d in data.iter() {
-                    (&d[..]).hash(state);
-                }
-            }
-        }
     }
 }
 
@@ -174,6 +71,16 @@ macro_rules! impl_array_newtype_show {
 
 macro_rules! impl_index_newtype {
     ($thing:ident, $ty:ty) => {
+
+        impl ::std::ops::Index<usize> for $thing {
+            type Output = $ty;
+
+            #[inline]
+            fn index(&self, index: usize) -> &$ty {
+                &self.0[index]
+            }
+        }
+
         impl ::std::ops::Index<::std::ops::Range<usize>> for $thing {
             type Output = [$ty];
 
@@ -705,14 +612,6 @@ macro_rules! user_enum {
         $(#[$attr])*
         pub enum $name {
             $(#[$doc] $elem),*
-        }
-
-        impl ::std::fmt::Debug for $name {
-            fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-                f.pad(match *self {
-                    $($name::$elem => $txt),*
-                })
-            }
         }
 
         impl ::std::fmt::Display for $name {
