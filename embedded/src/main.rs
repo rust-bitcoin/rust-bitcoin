bba57d7d69aaeb1a8d0a9c990ba6c326bf24a396
Devrandom <c1.devrandom@niftybox.net>
2021-05-15 17:12:27 +0200

    Embedded test via qemu

---
+++ embedded/src/main.rs
@@ -0,0 +1,76 @@
+#![feature(alloc_error_handler)]
+#![feature(panic_info_message)]
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+extern crate bitcoin;
+
+use alloc::string::ToString;
+use alloc::vec;
+use core::alloc::Layout;
+use core::panic::PanicInfo;
+
+use alloc_cortex_m::CortexMHeap;
+// use panic_halt as _;
+use bitcoin::{Address, Network, PrivateKey};
+use bitcoin::secp256k1::ffi::types::AlignedType;
+use bitcoin::secp256k1::Secp256k1;
+
+use cortex_m::asm;
+use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
+
+// this is the allocator the application will use
+#[global_allocator]
+static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+
+const HEAP_SIZE: usize = 1024 * 512; // 512 KB
+
+#[entry]
+fn main() -> ! {
+    hprintln!("heap size {}", HEAP_SIZE).unwrap();
+
+    unsafe { ALLOCATOR.init(cortex_m_rt::heap_start() as usize, HEAP_SIZE) }
+
+    let size = Secp256k1::preallocate_size();
+    hprintln!("secp buf size {}", size*16).unwrap();
+
+    // Load a private key
+    let raw = "L1HKVVLHXiUhecWnwFYF6L3shkf1E12HUmuZTESvBXUdx3yqVP1D";
+    let pk = PrivateKey::from_wif(raw).unwrap();
+    hprintln!("Seed WIF: {}", pk).unwrap();
+
+    let mut buf_ful = vec![AlignedType::zeroed(); size];
+    let secp = Secp256k1::preallocated_new(&mut buf_ful).unwrap();
+
+    // Derive address
+    let pubkey = pk.public_key(&secp);
+    let address = Address::p2wpkh(&pubkey, Network::Bitcoin).unwrap();
+    hprintln!("Address: {}", address).unwrap();
+
+    assert_eq!(address.to_string(), "bc1qpx9t9pzzl4qsydmhyt6ctrxxjd4ep549np9993".to_string());
+    // exit QEMU
+    // NOTE do not run this on hardware; it can corrupt OpenOCD state
+    debug::exit(debug::EXIT_SUCCESS);
+
+    loop {}
+}
+
+// define what happens in an Out Of Memory (OOM) condition
+#[alloc_error_handler]
+fn alloc_error(_layout: Layout) -> ! {
+    hprintln!("alloc error").unwrap();
+    debug::exit(debug::EXIT_FAILURE);
+    asm::bkpt();
+
+    loop {}
+}
+
+#[inline(never)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    hprintln!("panic {:?}", info.message()).unwrap();
+    debug::exit(debug::EXIT_FAILURE);
+    loop {}
+}
