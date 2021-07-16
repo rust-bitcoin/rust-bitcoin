877dc473070301e8b0ce6f88413a9ab3f688a69f
Riccardo Casatta <riccardo@casatta.it>
2021-07-16 10:51:11 +0200

    Add Reserved variant to SigHashType for future use (ie SIGHASH_ANYPREVOUT)

--- src/util/sighash.rs
+++ src/util/sighash.rs
@@ -110,6 +110,9 @@ pub enum SigHashType {
     NonePlusAnyoneCanPay = 0x82,
     /// 0x83: Sign one output and only this input (see `Single` for what "one output" means)
     SinglePlusAnyoneCanPay = 0x83,
+
+    /// Reserved for future use
+    Reserved = 0xFF,
 }
 
 /// Possible errors in computing the signature message
@@ -221,6 +224,7 @@ impl SigHashType {
             SigHashType::AllPlusAnyoneCanPay => (SigHashType::All, true),
             SigHashType::NonePlusAnyoneCanPay => (SigHashType::None, true),
             SigHashType::SinglePlusAnyoneCanPay => (SigHashType::Single, true),
+            SigHashType::Reserved => (SigHashType::Reserved, false),
         }
     }
 }
