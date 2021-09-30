0e1b99359cdfd4eef3f505d6353341503e2adc24
Martin Habovstiak <martin.habovstiak@gmail.com>
2021-09-30 14:35:05 +0200

    Added fuzz test for `Script::bytes_to_asm_fmt`
    This adds fuzz target for `Script::bytes_to_asm_fmt` which could
    panic due to overflow in the past. Fuzzing should decrease the risk of
    other panics.

---
+++ fuzz/fuzz_targets/script_bytes_to_asm_fmt.rs
@@ -0,0 +1,41 @@
+extern crate bitcoin;
+
+use std::fmt;
+
+// faster than String, we don't need to actually produce the value, just check absence of panics
+struct NullWriter;
+
+impl fmt::Write for NullWriter {
+    fn write_str(&mut self, _s: &str) -> fmt::Result {
+        Ok(())
+    }
+
+    fn write_char(&mut self, _c: char) -> fmt::Result {
+        Ok(())
+    }
+}
+
+fn do_test(data: &[u8]) {
+    let mut writer = NullWriter;
+    bitcoin::Script::bytes_to_asm_fmt(data, &mut writer);
+}
+
+#[cfg(feature = "afl")]
+#[macro_use] extern crate afl;
+#[cfg(feature = "afl")]
+fn main() {
+    fuzz!(|data| {
+        do_test(&data);
+    });
+}
+
+#[cfg(feature = "honggfuzz")]
+#[macro_use] extern crate honggfuzz;
+#[cfg(feature = "honggfuzz")]
+fn main() {
+    loop {
+        fuzz!(|data| {
+            do_test(data);
+        });
+    }
+}
