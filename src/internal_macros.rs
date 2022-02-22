c009210d4c0aef71f4d31f3bc41c3716e25f3871
Tobin Harding <me@tobin.cc>
2022-02-22 20:13:24 +0000

    Use full path for String in macro
    As is done in the rest of the `internal_macros` module use the fully
    qualified path for the `String` type.
    
    Done in preparation for using `serde_string_impl` in the `sighash`
    module.

--- src/internal_macros.rs
+++ src/internal_macros.rs
@@ -202,7 +202,7 @@ macro_rules! serde_string_impl {
                         self.visit_str(v)
                     }
 
-                    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+                    fn visit_string<E>(self, v: $crate::prelude::String) -> Result<Self::Value, E>
                     where
                         E: $crate::serde::de::Error,
                     {
@@ -264,7 +264,7 @@ macro_rules! serde_struct_human_string_impl {
                             self.visit_str(v)
                         }
 
-                        fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+                        fn visit_string<E>(self, v: $crate::prelude::String) -> Result<Self::Value, E>
                         where
                             E: $crate::serde::de::Error,
                         {
