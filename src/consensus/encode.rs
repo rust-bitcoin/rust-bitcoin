15d380012b9c19dadb4dba0e408fbdef5dd39d6d
Tobin Harding <me@tobin.cc>
2022-04-18 10:03:20 +1000

    Use assert_eq for encoded data length check
    According to the Rust docs for `debug_assert` it should only be used if
    there a proved performance costs to using `assert`.
    
    Checking the length of a vector is fast, therefore its unlikely that
    using `assert_eq` will have any noticeable performance costs.

--- src/consensus/encode.rs
+++ src/consensus/encode.rs
@@ -140,7 +140,7 @@ impl From<psbt::Error> for Error {
 pub fn serialize<T: Encodable + ?Sized>(data: &T) -> Vec<u8> {
     let mut encoder = Vec::new();
     let len = data.consensus_encode(&mut encoder).expect("in-memory writers don't error");
-    debug_assert_eq!(len, encoder.len());
+    assert_eq!(len, encoder.len());
     encoder
 }
 
