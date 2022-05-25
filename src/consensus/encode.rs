51c60b8507f8ac91b828155a4ab591af1d905627
Tobin C. Harding <me@tobin.cc>
2022-05-25 13:46:56 +1000

    Allow no is_empty method for VarInt
    Clippy emits:
    
      warning: struct `VarInt` has a public `len` method, but no `is_empty`
      method
    
    However, `VarInt` has no concept of 'is empty' so add a compiler
    directive to allow the lint.

--- src/consensus/encode.rs
+++ src/consensus/encode.rs
@@ -359,6 +359,7 @@ impl_int_encodable!(i16, read_i16, emit_i16);
 impl_int_encodable!(i32, read_i32, emit_i32);
 impl_int_encodable!(i64, read_i64, emit_i64);
 
+#[allow(clippy::len_without_is_empty)] // VarInt has on concept of 'is_empty'.
 impl VarInt {
     /// Gets the length of this VarInt when encoded.
     /// Returns 1 for 0..=0xFC, 3 for 0xFD..=(2^16-1), 5 for 0x10000..=(2^32-1),
