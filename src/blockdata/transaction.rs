91ca7ecd525340a9079f3db32e1b0a5a770ea675
Martin Habovstiak <martin.habovstiak@gmail.com>
2022-07-28 22:50:32 +0200

    Basic testing with `check_io`
    This implements a few tests using the `check_io` crate.

--- src/blockdata/transaction.rs
+++ src/blockdata/transaction.rs
@@ -1266,7 +1266,6 @@ mod tests {
     use crate::blockdata::constants::WITNESS_SCALE_FACTOR;
     use crate::blockdata::script::Script;
     use crate::blockdata::locktime::PackedLockTime;
-    use crate::consensus::encode::serialize;
     use crate::consensus::encode::deserialize;
 
     use crate::hashes::Hash;
@@ -1275,6 +1274,7 @@ mod tests {
     use crate::hash_types::*;
     use super::EcdsaSighashType;
     use crate::util::sighash::SighashCache;
+    use io_check::{test_read, test_write};
 
     const SOME_TX: &str = "0100000001a15d57094aa7a21a28cb20b59aab8fc7d1149a3bdbcddba9c622e4f5f6a99ece010000006c493046022100f93bb0e7d8db7bd46e40132d1f8242026e045f03a0efe71bbb8e3f475e970d790221009337cd7f1f929f00cc6ff01f03729b069a7c21b59b1736ddfee5db5946c5da8c0121033b9b137ee87d5a812d6f506efdd37f0affa7ffc310711c06c7f3e097c9447c52ffffffff0100e1f505000000001976a9140389035a9225b3839e2bbf32d826a1e222031fd888ac00000000";
 
@@ -1446,13 +1446,13 @@ mod tests {
         let tx_bytes = Vec::from_hex(
             "010000000001000100e1f505000000001976a9140389035a9225b3839e2bbf32d826a1e222031fd888ac00000000"
         ).unwrap();
-        let tx: Transaction = deserialize(&tx_bytes).expect("deserialize tx");
+        test_read(&tx_bytes, |mut reader| {
+            let tx = Transaction::consensus_decode(&mut reader).unwrap();
+            assert_eq!(tx.input.len(), 0);
+            assert_eq!(tx.output.len(), 1);
 
-        assert_eq!(tx.input.len(), 0);
-        assert_eq!(tx.output.len(), 1);
-
-        let reser = serialize(&tx);
-        assert_eq!(tx_bytes, reser);
+            test_write(&tx_bytes, |mut writer| { tx.consensus_encode(&mut writer).unwrap(); });
+        });
     }
 
     #[test]
@@ -1530,6 +1530,7 @@ mod tests {
         let tx_bytes = Vec::from_hex("0100000001a15d57094aa7a21a28cb20b59aab8fc7d1149a3bdbcddba9c622e4f5f6a99ece010000006c493046022100f93bb0e7d8db7bd46e40132d1f8242026e045f03a0efe71bbb8e3f475e970d790221009337cd7f1f929f00cc6ff01f03729b069a7c21b59b1736ddfee5db5946c5da8c0121033b9b137ee87d5a812d6f506efdd37f0affa7ffc310711c06c7f3e097c9447c52ffffffff0100e1f505000000001976a9140389035a9225b3839e2bbf32d826a1e222031fd888ac00000000").unwrap();
         let tx: Transaction = deserialize(&tx_bytes).unwrap();
         serde_round_trip!(tx);
+        test_write(&tx_bytes, |mut writer| { tx.consensus_encode(&mut writer).unwrap(); });
     }
 
     // Test decoding transaction `4be105f158ea44aec57bf12c5817d073a712ab131df6f37786872cfc70734188`
@@ -1538,12 +1539,14 @@ mod tests {
     #[cfg(feature = "serde")]
     fn test_segwit_tx_decode() {
         let tx_bytes = Vec::from_hex("010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff3603da1b0e00045503bd5704c7dd8a0d0ced13bb5785010800000000000a636b706f6f6c122f4e696e6a61506f6f6c2f5345475749542fffffffff02b4e5a212000000001976a914876fbb82ec05caa6af7a3b5e5a983aae6c6cc6d688ac0000000000000000266a24aa21a9edf91c46b49eb8a29089980f02ee6b57e7d63d33b18b4fddac2bcd7db2a39837040120000000000000000000000000000000000000000000000000000000000000000000000000").unwrap();
-        let tx: Transaction = deserialize(&tx_bytes).unwrap();
-        assert_eq!(tx.weight(), 780);
-        serde_round_trip!(tx);
+        test_read(&tx_bytes, |mut reader| {
+            let tx = Transaction::consensus_decode(&mut reader).unwrap();
+            assert_eq!(tx.weight(), 780);
 
-        let consensus_encoded = serialize(&tx);
-        assert_eq!(consensus_encoded, tx_bytes);
+            serde_round_trip!(tx);
+
+            test_write(&tx_bytes, |mut writer| { tx.consensus_encode(&mut writer).unwrap(); });
+        });
     }
 
     #[test]
