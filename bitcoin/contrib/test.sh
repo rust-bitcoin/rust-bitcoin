022730bd8d740b877befa611046c99cd7ceeebfe
Tobin C. Harding <me@tobin.cc>
2022-09-08 09:32:08 +1000

    Add a workspace to the top level directory.
    Create a directory `bitcoin` and move into it the following as is with
    no code changes:
    
    - src
    - Cargo.toml
    - contrib
    - test_data
    - examples
    
    Then do:
    
    - Add a workspace to the repository root directory.
    - Add the newly created `bitcoin` crate to the workspace.
    - Exclude `fuzz` and `embedded` crates from the workspace.
    - Add a contrib/test.sh script that runs contrib/test.sh in each
      sub-crate
    - Fix the bitcoin/contrib/test.sh script

---
+++ bitcoin/contrib/test.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+
+set -ex
+
+FEATURES="base64 bitcoinconsensus serde rand secp-recovery"
+
+if [ "$DO_COV" = true ]
+then
+    export RUSTFLAGS="-C link-dead-code"
+fi
+
+cargo --version
+rustc --version
+
+# Work out if we are using a nightly toolchain.
+NIGHTLY=false
+if cargo --version | grep nightly; then
+    NIGHTLY=true
+fi
+
+# We should not have any duplicate dependencies. This catches mistakes made upgrading dependencies
+# in one crate and not in another (e.g. upgrade bitcoin_hashes in bitcoin but not in secp).
+cargo update -p serde --precise 1.0.142
+cargo update -p serde_test --precise 1.0.142
+cargo update -p serde_derive --precise 1.0.142
+duplicate_dependencies=$(cargo tree  --target=all --all-features --duplicates | wc -l)
+if [ "$duplicate_dependencies" -ne 0 ]; then
+    echo "Dependency tree is broken, contains duplicates"
+    cargo tree  --target=all --all-features --duplicates
+    exit 1
+fi
+
+if [ "$DO_LINT" = true ]
+then
+    cargo clippy --all-features --all-targets -- -D warnings
+    cargo clippy --example bip32 -- -D warnings
+    cargo clippy --example handshake -- -D warnings
+    cargo clippy --example ecdsa-psbt --features=bitcoinconsensus -- -D warnings
+fi
+
+echo "********* Testing std *************"
+# Test without any features other than std first
+cargo test --verbose --no-default-features --features="std"
+
+echo "********* Testing default *************"
+# Then test with the default features
+cargo test --verbose
+
+if [ "$DO_NO_STD" = true ]
+then
+    echo "********* Testing no-std build *************"
+    # Build no_std, to make sure that cfg(test) doesn't hide any issues
+    cargo build --verbose --features="no-std" --no-default-features
+
+    # Build std + no_std, to make sure they are not incompatible
+    cargo build --verbose --features="no-std"
+
+    # Test no_std
+    cargo test --verbose --features="no-std" --no-default-features
+
+    # Build all features
+    cargo build --verbose --features="no-std $FEATURES" --no-default-features
+
+    # Build specific features
+    for feature in ${FEATURES}
+    do
+        cargo build --verbose --features="no-std $feature"
+    done
+
+    cargo run --example bip32 7934c09359b234e076b9fa5a1abfd38e3dc2a9939745b7cc3c22a48d831d14bd
+    cargo run --no-default-features --features no-std --example bip32 7934c09359b234e076b9fa5a1abfd38e3dc2a9939745b7cc3c22a48d831d14bd
+fi
+
+# Test each feature
+for feature in ${FEATURES}
+do
+    echo "********* Testing $feature *************"
+    cargo test --verbose --features="$feature"
+done
+
+cargo run --example ecdsa-psbt --features=bitcoinconsensus
+
+# Build the docs if told to (this only works with the nightly toolchain)
+if [ "$DO_DOCS" = true ]; then
+    RUSTDOCFLAGS="--cfg docsrs" cargo +nightly rustdoc --features="$FEATURES" -- -D rustdoc::broken-intra-doc-links
+fi
+
+# Fuzz if told to
+if [ "$DO_FUZZ" = true ]
+then
+    (
+        cd fuzz
+        cargo test --verbose
+        ./travis-fuzz.sh
+    )
+fi
+
+# Bench if told to, only works with non-stable toolchain (nightly, beta).
+if [ "$DO_BENCH" = true ]
+then
+    if [ "$NIGHTLY" = false ]
+    then
+        if [ -n "$RUSTUP_TOOLCHAIN" ]
+        then
+            echo "RUSTUP_TOOLCHAIN is set to a non-nightly toolchain but DO_BENCH requires a nightly toolchain"
+        else
+            echo "DO_BENCH requires a nightly toolchain"
+        fi
+        exit 1
+    fi
+    RUSTFLAGS='--cfg=bench' cargo bench
+fi
+
+# Use as dependency if told to
+if [ "$AS_DEPENDENCY" = true ]
+then
+    cargo new dep_test 2> /dev/null # Mute warning about workspace, fixed below.
+    cd dep_test
+    echo 'bitcoin = { path = "..", features = ["serde"] }\n\n' >> Cargo.toml
+    # Adding an empty workspace section excludes this crate from the rust-bitcoin workspace.
+    echo '[workspace]\n\n' >> Cargo.toml
+
+    cargo update -p serde --precise 1.0.142
+    cargo update -p serde_derive --precise 1.0.142
+
+    cargo test --verbose
+fi
