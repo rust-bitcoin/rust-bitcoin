1bf885550e773911a5a6ecbcf496264ad3a0db4b
Martin Habovstiak <martin.habovstiak@gmail.com>
2022-09-14 19:30:15 +0200

    Redesign `hex::BufEncoder` to accept owned arrays
    Not being able to create an owned `BufEncoder` prevented returning it
    from functions which need to allocate the buffer on stack. Such is the
    case in WIP serde via consensus serialzation.
    
    This change refactors `OutBytes` to be unsized, adds an `AsOutBytes`
    trait and uses that one instead of `Into` to perform the conversion.

--- internals/src/lib.rs
+++ internals/src/lib.rs
@@ -11,7 +11,6 @@
 // Experimental features we need.
 #![cfg_attr(docsrs, feature(doc_cfg))]
 // Coding conventions
-#![forbid(unsafe_code)]
 #![deny(non_upper_case_globals)]
 #![deny(non_camel_case_types)]
 #![deny(non_snake_case)]
