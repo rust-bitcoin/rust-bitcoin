783e1e81dcf2ccc601c1615277386f1b10ab6cc2
Martin Habovstiak <martin.habovstiak@gmail.com>
2022-09-20 12:08:06 +0200

    Move `impl_std_error` to `bitcoin-internals`
    The macro is useful for all other crates thus it is moved to the
    internals crate in this commit.

--- bitcoin/src/error.rs
+++ bitcoin/src/error.rs
@@ -2,22 +2,6 @@
 
 //! Contains error types and other error handling tools.
 
-pub use crate::parse::ParseIntError;
+pub(crate) use internals::impl_std_error;
 
-/// Impls std::error::Error for the specified type with appropriate attributes, possibly returning
-/// source.
-macro_rules! impl_std_error {
-    // No source available
-    ($type:ty) => {
-        #[cfg(feature = "std")]
-        impl std::error::Error for $type {}
-    };
-    // Struct with $field as source
-    ($type:ty, $field:ident) => {
-        #[cfg(feature = "std")]
-        impl std::error::Error for $type {
-            fn source(&self) -> Option<&(dyn std::error::Error + 'static)> { Some(&self.$field) }
-        }
-    };
-}
-pub(crate) use impl_std_error;
+pub use crate::parse::ParseIntError;
