4ce99107122e8b3813271dd1aa264ad4bcfdb6c8
Tobin C. Harding <me@tobin.cc>
2023-01-22 12:58:24 +1100

    hex: Add exports and basic usage
    Add a basic usage rustdoc example to the `hex` crate. While we are at it
    add a few extra re-exports to make the crate more ergonomic to work with.

--- hex/src/lib.rs
+++ hex/src/lib.rs
@@ -2,6 +2,26 @@
 // SPDX-License-Identifier: CC0-1.0
 
 //! This is a hex library designed to be fast.
+//!
+//! ## Basic Usage
+//! ```
+//! # #[cfg(feature = "alloc")]
+//! # {
+//! // Use the `package` key to improve import ergonomics (`hex` instead of `bitcoin-hex`).
+//! // hex = { package = "bitcoin-hex", version = "*" }
+//! # use bitcoin_hex as hex; // No need for this if using `package` as above.
+//! use hex::{DisplayHex, FromHex};
+//!
+//! // Decode an arbitrary length hex string into a vector.
+//! let v = Vec::from_hex("deadbeef").expect("valid hex digits");
+//! // Or a known length hex string into a fixed size array.
+//! let a = <[u8; 4]>::from_hex("deadbeef").expect("valid length and valid hex digits");
+//! // We support `LowerHex` and `UpperHex` out of the box for `[u8]` slices.
+//! println!("An array as lower hex: {:x}", a.as_hex());
+//! // And for vecs since `Vec` derefs to byte slice.
+//! println!("A vector as upper hex: {:X}", v.as_hex());
+//! # }
+//! ```
 
 // Experimental features we need.
 #![cfg_attr(docsrs, feature(doc_cfg))]
@@ -14,9 +34,13 @@ pub mod buf_encoder;
 pub mod display;
 pub mod parse;
 
+pub use display::DisplayHex;
+pub use parse::{Error, FromHex};
+
 /// Reexports of extension traits.
 pub mod exts {
     pub use super::display::DisplayHex;
+    pub use super::parse::FromHex;
 }
 
 /// Mainly reexports based on features.
