673ca2d2fe8aca512c3b2a860727745a1b1a376a
Lorenzo Maturano <maturanolorenzo@gmail.com>
2023-02-21 14:22:44 -0300

    changing docs and examples to use reference to slice in `derive_pub`

--- bitcoin/src/bip32.rs
+++ bitcoin/src/bip32.rs
@@ -683,7 +683,7 @@ impl ExtendedPubKey {
 
     /// Attempts to derive an extended public key from a path.
     ///
-    /// The `path` argument can be both of type `DerivationPath` or `Vec<ChildNumber>`.
+    /// The `path` argument can be both of type `DerivationPath` or `AsRef<ChildNumber>`.
     pub fn derive_pub<C: secp256k1::Verification, P: AsRef<[ChildNumber]>>(
         &self,
         secp: &Secp256k1<C>,
