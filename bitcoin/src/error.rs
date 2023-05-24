0f74eb68762865b7612dfc6656701b753deccf77
Tobin C. Harding <me@tobin.cc>
2023-05-24 13:54:24 +1000

    Remove the unused crate::Error
    The `crate::Error` type is now unused, remove it.

--- bitcoin/src/error.rs
+++ bitcoin/src/error.rs
@@ -2,45 +2,7 @@
 
 //! Contains error types and other error handling tools.
 
-use core::fmt;
-
-use internals::write_err;
-
-use crate::consensus::encode;
 pub use crate::parse::ParseIntError;
-
-/// A general error code, other errors should implement conversions to/from this
-/// if appropriate.
-#[derive(Debug)]
-#[non_exhaustive]
-pub enum Error {
-    /// Encoding error
-    Encode(encode::Error),
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::Encode(ref e) => write_err!(f, "encoding error"; e),
-        }
-    }
-}
-
-#[cfg(feature = "std")]
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        use self::Error::*;
-
-        match self {
-            Encode(e) => Some(e),
-        }
-    }
-}
-
-#[doc(hidden)]
-impl From<encode::Error> for Error {
-    fn from(e: encode::Error) -> Error { Error::Encode(e) }
-}
 
 /// Impls std::error::Error for the specified type with appropriate attributes, possibly returning
 /// source.
