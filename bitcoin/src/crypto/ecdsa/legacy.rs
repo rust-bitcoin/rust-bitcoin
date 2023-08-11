9cc93cdb026000fec13d220e06cdc97ad4e74f4a
Tobin C. Harding <me@tobin.cc>
2023-08-11 13:35:04 +1000

    ecdsa: Add Legacy and SegwitV0 signatures
    Add two types `ecdsa::LegacySignature` and `ecdsa::SegwitV0Signature`.
    both are just thin wrappers around `ecdsa::Signature`.
    
    Do not use the new types anywhere.

---
+++ bitcoin/src/crypto/ecdsa/legacy.rs
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: CC0-1.0
+
+//! Legacy ECDSA Bitcoin signatures.
+
+use core::fmt;
+use core::str::FromStr;
+
+use secp256k1;
+
+#[cfg(doc)]
+use crate::crypto::ecdsa::EcdsaSighashType;
+use crate::crypto::ecdsa::{self, Error, SerializedSignature};
+use crate::prelude::*;
+
+/// A legacy Bitcoin ECDSA signature.
+///
+/// This is just a thin wrapper around [`ecdsa::Signature`].
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde", serde(crate = "actual_serde"))]
+pub struct LegacySignature(pub(crate) ecdsa::Signature);
+
+impl LegacySignature {
+    /// Construct a legacy ECDSA Bitcoin signature for [`EcdsaSighashType::All`].
+    pub fn sighash_all(sig: secp256k1::ecdsa::Signature) -> LegacySignature {
+        Self(ecdsa::Signature::sighash_all(sig))
+    }
+
+    /// Deserializes from slice following the standardness rules for [`EcdsaSighashType`].
+    pub fn from_slice(sl: &[u8]) -> Result<Self, Error> {
+        Ok(Self(ecdsa::Signature::from_slice(sl)?))
+    }
+
+    /// Serializes a legacy ECDSA signature (inner secp256k1 signature in DER format).
+    ///
+    /// This does **not** perform extra heap allocation.
+    pub fn serialize(&self) -> SerializedSignature { self.0.serialize() }
+
+    /// Serializes a legacy ECDSA signature (inner secp256k1 signature in DER format) into `Vec`.
+    ///
+    /// Note: this performs an extra heap allocation, you might prefer the
+    /// [`serialize`](Self::serialize) method instead.
+    pub fn to_vec(self) -> Vec<u8> { self.0.to_vec() }
+}
+
+impl fmt::Display for LegacySignature {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { fmt::Display::fmt(&self.0, f) }
+}
+
+impl FromStr for LegacySignature {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> { Ok(Self(ecdsa::Signature::from_str(s)?)) }
+}
---
+++ bitcoin/src/crypto/ecdsa/segwit_v0.rs
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: CC0-1.0
+
+//! Segwit v0 ECDSA Bitcoin signatures.
+
+use core::fmt;
+use core::str::FromStr;
+
+use secp256k1;
+
+#[cfg(doc)]
+use crate::crypto::ecdsa::EcdsaSighashType;
+use crate::crypto::ecdsa::{self, Error, SerializedSignature};
+use crate::prelude::*;
+
+/// A segwit v0 Bitcoin ECDSA signature.
+///
+/// This is just a thin wrapper around [`ecdsa::Signature`].
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde", serde(crate = "actual_serde"))]
+pub struct SegwitV0Signature(pub(crate) ecdsa::Signature);
+
+impl SegwitV0Signature {
+    /// Construct a segwit v0 ECDSA Bitcoin signature for [`EcdsaSighashType::All`].
+    pub fn sighash_all(sig: secp256k1::ecdsa::Signature) -> SegwitV0Signature {
+        Self(ecdsa::Signature::sighash_all(sig))
+    }
+
+    /// Deserializes from slice following the standardness rules for [`EcdsaSighashType`].
+    pub fn from_slice(sl: &[u8]) -> Result<Self, Error> {
+        Ok(Self(ecdsa::Signature::from_slice(sl)?))
+    }
+
+    /// Serializes a segwit v0 ECDSA signature (inner secp256k1 signature in DER format).
+    ///
+    /// This does **not** perform extra heap allocation.
+    pub fn serialize(&self) -> SerializedSignature { self.0.serialize() }
+
+    /// Serializes a segwit v0 ECDSA signature (inner secp256k1 signature in DER format) into `Vec`.
+    ///
+    /// Note: this performs an extra heap allocation, you might prefer the
+    /// [`serialize`](Self::serialize) method instead.
+    pub fn to_vec(self) -> Vec<u8> { self.0.to_vec() }
+}
+
+impl fmt::Display for SegwitV0Signature {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { fmt::Display::fmt(&self.0, f) }
+}
+
+impl FromStr for SegwitV0Signature {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> { Ok(Self(ecdsa::Signature::from_str(s)?)) }
+}
