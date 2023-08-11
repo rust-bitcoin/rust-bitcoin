27ba228597eb3b2be05ec9a8f1558b7919c79dc5
Tobin C. Harding <me@tobin.cc>
2023-08-11 12:10:15 +1000

    Move ecdsa error code to submodule
    Move the error code out of the `ecdsa/mod.rs` file and into a private
    `error` submodule. Add public re-export so this not a breaking change.
    
    Code move only plus import/export changes to build cleanly. No other
    changes.

--- bitcoin/src/crypto/ecdsa/mod.rs
+++ bitcoin/src/crypto/ecdsa/mod.rs
@@ -8,12 +8,14 @@ use core::str::FromStr;
 use core::{fmt, iter};
 
 use hex::FromHex;
-use internals::write_err;
 use secp256k1;
 
 use crate::prelude::*;
 use crate::script::PushBytes;
-use crate::sighash::{EcdsaSighashType, NonStandardSighashTypeError};
+use crate::sighash::EcdsaSighashType;
+
+mod error;
+pub use self::error::Error;
 
 const MAX_SIG_LEN: usize = 73;
 
@@ -180,57 +182,4 @@ impl<'a> IntoIterator for &'a SerializedSignature {
 
     #[inline]
     fn into_iter(self) -> Self::IntoIter { (*self).iter() }
-}
-
-/// An ECDSA signature-related error.
-#[derive(Debug, Clone, PartialEq, Eq)]
-#[non_exhaustive]
-pub enum Error {
-    /// Hex decoding error.
-    Hex(hex::HexToBytesError),
-    /// Non-standard sighash type.
-    SighashType(NonStandardSighashTypeError),
-    /// Signature was empty.
-    EmptySignature,
-    /// A secp256k1 error.
-    Secp256k1(secp256k1::Error),
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use Error::*;
-
-        match *self {
-            Hex(ref e) => write_err!(f, "signature hex decoding error"; e),
-            SighashType(ref e) => write_err!(f, "non-standard signature hash type"; e),
-            EmptySignature => write!(f, "empty ECDSA signature"),
-            Secp256k1(ref e) => write_err!(f, "secp256k1"; e),
-        }
-    }
-}
-
-#[cfg(feature = "std")]
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        use self::Error::*;
-
-        match self {
-            Hex(e) => Some(e),
-            Secp256k1(e) => Some(e),
-            SighashType(e) => Some(e),
-            EmptySignature => None,
-        }
-    }
-}
-
-impl From<secp256k1::Error> for Error {
-    fn from(e: secp256k1::Error) -> Error { Error::Secp256k1(e) }
-}
-
-impl From<NonStandardSighashTypeError> for Error {
-    fn from(err: NonStandardSighashTypeError) -> Self { Error::SighashType(err) }
-}
-
-impl From<hex::HexToBytesError> for Error {
-    fn from(err: hex::HexToBytesError) -> Self { Error::Hex(err) }
 }
