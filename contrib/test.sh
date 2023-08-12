02890d3fbafac689c063eebc41f7094e262e47e0
yancy <email@yancy.lol>
2023-08-12 11:07:15 +0200

    Use spaces instead of tabs

--- contrib/test.sh
+++ contrib/test.sh
@@ -22,25 +22,25 @@ fi
 
 for dep in $DEPS
 do
-	cp "Cargo-$dep.lock" Cargo.lock
-	for crate in ${CRATES}
-	do
-	    (
-		cd "$crate"
-		./contrib/test.sh
-	    )
-	done
-	if [ "$dep" = recent ];
-	then
-		# We always test committed dependencies but we want to warn if they could've been updated
-		cargo update
-		if diff Cargo-recent.lock Cargo.lock;
-		then
-			echo Dependencies are up to date
-		else
-			echo "::warning file=Cargo-recent.lock::Dependencies could be updated"
-		fi
-	fi
+    cp "Cargo-$dep.lock" Cargo.lock
+    for crate in ${CRATES}
+    do
+        (
+            cd "$crate"
+            ./contrib/test.sh
+        )
+    done
+    if [ "$dep" = recent ];
+    then
+        # We always test committed dependencies but we want to warn if they could've been updated
+        cargo update
+        if diff Cargo-recent.lock Cargo.lock;
+        then
+            echo Dependencies are up to date
+        else
+            echo "::warning file=Cargo-recent.lock::Dependencies could be updated"
+        fi
+    fi
 done
 
 exit 0
