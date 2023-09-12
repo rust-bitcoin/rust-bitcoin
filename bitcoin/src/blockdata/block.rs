5e0209569c1eb4005c08fb6da9387a95203480b7
Matt Corallo <git@bluematt.me>
2023-09-12 17:45:08 +0000

    Use `io::sink` rather than our custom `EmptyWrite` utility

--- bitcoin/src/blockdata/block.rs
+++ bitcoin/src/blockdata/block.rs
@@ -637,7 +637,7 @@ mod benches {
 
     use super::Block;
     use crate::consensus::{deserialize, Decodable, Encodable};
-    use crate::EmptyWrite;
+    use crate::io::sink;
 
     #[bench]
     pub fn bench_stream_reader(bh: &mut Bencher) {
@@ -674,7 +674,7 @@ mod benches {
         let block: Block = deserialize(&raw_block[..]).unwrap();
 
         bh.iter(|| {
-            let size = block.consensus_encode(&mut EmptyWrite);
+            let size = block.consensus_encode(&mut sink());
             black_box(&size);
         });
     }
