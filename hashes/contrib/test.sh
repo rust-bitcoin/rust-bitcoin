685e5101ce968d8fe8e1b41066aa2c4a0c7ad25f
Tobin C. Harding <me@tobin.cc>
2023-10-04 11:06:50 +1100

    Add wasm dev-deps using CI script
    We only test WASM in CI using a stable toolchain however because we have
    a target specific dev-dependencies section the wasm deps get pulled in
    during MSRV builds - this breaks the MSRV build.
    
    Instead of including WASM dev-dependencies in the manifest we can
    dynamically modify the manifest when running the WASM tests. We do this
    already to add the `crate-type` section so this is not really that
    surprising to see in the CI script.
    
    Doing so allows us to stop pinning the transitive `syn` dependency also
    which is included in the dependency graph because of `wasm-bingen-test`.

--- hashes/contrib/test.sh
+++ hashes/contrib/test.sh
@@ -78,6 +78,7 @@ fi
 if [ "$DO_WASM" = true ]; then
     clang --version &&
     CARGO_TARGET_DIR=wasm cargo install --force wasm-pack &&
+    printf '\n[target.wasm32-unknown-unknown.dev-dependencies]\nwasm-bindgen-test = "0.3"\n' >> Cargo.toml &&
     printf '\n[lib]\ncrate-type = ["cdylib", "rlib"]\n' >> Cargo.toml &&
     CC=clang-9 wasm-pack build &&
     CC=clang-9 wasm-pack test --node;
