801c72e0566e38f98fe93a43074f4a6db8b20958
Tobin C. Harding <me@tobin.cc>
2023-11-08 09:00:53 +1100

    Add deprecation comment to hash_types module
    The `hash_types` module has been emptied, it now contains only
    deprecated re-exports. Add a rustdoc comment stating as such.

--- bitcoin/src/hash_types.rs
+++ bitcoin/src/hash_types.rs
@@ -2,11 +2,7 @@
 
 //! Bitcoin hash types.
 //!
-//! This module defines types for hashes used throughout the library. These
-//! types are needed in order to avoid mixing data of the same hash format
-//! (e.g. `SHA256d`) but of different meaning (such as transaction id, block
-//! hash).
-//!
+//! This module is deprecated. You can find hash types in their respective, hopefully obvious, modules.
 
 #[deprecated(since = "0.0.0-NEXT-RELEASE", note = "use crate::T instead")]
 pub use crate::{
