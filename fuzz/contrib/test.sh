85cf7ac956f91209eb859bd2f9a25aa177b28d22
Tobin C. Harding <me@tobin.cc>
2024-01-11 15:07:16 +1100

    CI: Remove pinning
    These MSRV pinning blocks are stale, remove them.

--- fuzz/contrib/test.sh
+++ fuzz/contrib/test.sh
@@ -10,16 +10,6 @@ rustc --version
 # Make all cargo invocations verbose
 export CARGO_TERM_VERBOSE=true
 
-# Pin dependencies as required if we are using MSRV toolchain.
-if cargo --version | grep "1\.41"; then
-    # 1.0.157 uses syn 2.0 which requires edition 2021
-    cargo update -p serde --precise 1.0.156
-    # 1.0.108 uses `matches!` macro so does not work with Rust 1.41.1, bad `syn` no biscuit.
-    cargo update -p syn --precise 1.0.107
-    # Half 1.8 uses edition 2021 features
-    cargo update -p half --precise 1.7.1
-fi
-
 # Defaults / sanity checks
 cargo build
 cargo test
--- hashes/contrib/test.sh
+++ hashes/contrib/test.sh
@@ -13,13 +13,6 @@ if cargo --version | grep nightly >/dev/null; then
     NIGHTLY=true
 fi
 
-# Pin dependencies as required if we are using MSRV toolchain.
-if cargo --version | grep "1\.48"; then
-    # 1.0.157 uses syn 2.0 which requires edition 2021
-    cargo update -p serde_json --precise 1.0.99
-    cargo update -p serde --precise 1.0.156
-fi
-
 # Make all cargo invocations verbose
 export CARGO_TERM_VERBOSE=true
 
