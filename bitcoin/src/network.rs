5818e04328cba73a7dc72ae47374a59dd57768e9
Ava Chow <github@achow101.com>
2024-03-08 15:07:34 -0500

    Add ServiceFlags::P2P_V2

--- bitcoin/src/network.rs
+++ bitcoin/src/network.rs
@@ -347,6 +347,7 @@ mod tests {
             ServiceFlags::WITNESS,
             ServiceFlags::COMPACT_FILTERS,
             ServiceFlags::NETWORK_LIMITED,
+            ServiceFlags::P2P_V2,
         ];
 
         let mut flags = ServiceFlags::NONE;
