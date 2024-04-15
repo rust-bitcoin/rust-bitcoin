cc0a8a33da68b871deabdd0947640bb0ac86313c
Tobin C. Harding <me@tobin.cc>
2024-04-15 14:31:22 +1000

    Remove stale comment

--- hashes/src/util.rs
+++ hashes/src/util.rs
@@ -214,8 +214,7 @@ macro_rules! hash_newtype {
 
         impl $crate::_export::_core::convert::From<$hash> for $newtype {
             fn from(inner: $hash) -> $newtype {
-                // Due to rust 1.22 we have to use this instead of simple `Self(inner)`
-                Self { 0: inner }
+                Self(inner)
             }
         }
 
