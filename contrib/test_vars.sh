6def5bc974b1907e39c44d7e2144af3603a6c1e1
Tobin C. Harding <me@tobin.cc>
2024-05-02 13:08:20 +1000

    CI: Use run_task from maintainer tools
    Use the shiny new `run_task.sh` script from maintainer tools.
    
    This patch should not change the test coverage in any way.

--- contrib/test_vars.sh
+++ contrib/test_vars.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+#
+# Used by labeler.yaml
+#
+# Not to be confused with the per crate `test_vars.sh` used by
+# `rust-bitcoin-maintainer-tools-run_task.sh`.
 
 CRATES="`cargo metadata --no-deps --format-version 1 | jq -j -r '.packages | map(.manifest_path | rtrimstr("/Cargo.toml") | ltrimstr("'$PWD'/")) | join(" ")'`"
 DEPS="recent minimal"
