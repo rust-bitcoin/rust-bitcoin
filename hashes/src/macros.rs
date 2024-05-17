55ea59017033e3fbe0cb9d21b180c84dcd6a4be9
Tobin C. Harding <me@tobin.cc>
2024-05-17 11:41:43 +1000

    hashes: Pull stringly stuff out into a separate macro
    In preparation for feature gating hex stuff pull the bytelike traits
    that depend on `hex` (ie, stringly stuff) out into a separate macro and
    call the new macro from the original.
    
    Structural change only, no behavioural change.

--- hashes/src/macros.rs
+++ hashes/src/macros.rs
@@ -236,28 +236,7 @@ macro_rules! hash_newtype_struct {
 #[macro_export]
 macro_rules! impl_bytelike_traits {
     ($ty:ident, $len:expr, $reverse:expr $(, $gen:ident: $gent:ident)*) => {
-        impl<$($gen: $gent),*> $crate::_export::_core::str::FromStr for $ty<$($gen),*> {
-            type Err = $crate::hex::HexToArrayError;
-
-            fn from_str(s: &str) -> $crate::_export::_core::result::Result<Self, Self::Err> {
-                use $crate::hex::FromHex;
-
-                let mut bytes = <[u8; $len]>::from_hex(s)?;
-                if $reverse {
-                    bytes.reverse();
-                }
-                Ok(Self::from_byte_array(bytes))
-            }
-        }
-
-        $crate::hex::impl_fmt_traits! {
-            #[display_backward($reverse)]
-            impl<$($gen: $gent),*> fmt_traits for $ty<$($gen),*> {
-                const LENGTH: usize = $len;
-            }
-        }
-
-        $crate::serde_impl!($ty, $len $(, $gen: $gent)*);
+        $crate::impl_bytelike_stringly_traits!($ty, $len, $reverse $(, $gen: $gent)*);
 
         impl<$($gen: $gent),*> $crate::_export::_core::convert::AsRef<[u8; $len]> for $ty<$($gen),*> {
             #[inline]
@@ -280,6 +259,49 @@ macro_rules! impl_bytelike_traits {
             #[inline]
             fn index(&self, index: I) -> &Self::Output { &self.0[index] }
         }
+    }
+}
+
+/// Adds stringly trait impls for an bytelike type, requires "hex" feature.
+///
+/// Implements:
+///
+/// * `str::FromStr`
+/// * `fmt::{LowerHex, UpperHex}` using `hex-conservative`.
+/// * `fmt::{Display, Debug}` by calling `LowerHex`
+/// * `serde::{Deserialize, Serialize}`
+///
+/// Arguments:
+///
+/// * `ty` - The bytelike type to implement the traits on.
+/// * `$len` - The number of bytes this type has.
+/// * `$reverse` - `true` if the type should be displayed backwards, `false` otherwise.
+/// * `$gen: $gent` - generic type(s) and trait bound(s).
+#[macro_export]
+macro_rules! impl_bytelike_stringly_traits {
+    ($ty:ident, $len:expr, $reverse:expr $(, $gen:ident: $gent:ident)*) => {
+        impl<$($gen: $gent),*> $crate::_export::_core::str::FromStr for $ty<$($gen),*> {
+            type Err = $crate::hex::HexToArrayError;
+
+            fn from_str(s: &str) -> $crate::_export::_core::result::Result<Self, Self::Err> {
+                use $crate::hex::FromHex;
+
+                let mut bytes = <[u8; $len]>::from_hex(s)?;
+                if $reverse {
+                    bytes.reverse();
+                }
+                Ok(Self::from_byte_array(bytes))
+            }
+        }
+
+        $crate::hex::impl_fmt_traits! {
+            #[display_backward($reverse)]
+            impl<$($gen: $gent),*> fmt_traits for $ty<$($gen),*> {
+                const LENGTH: usize = $len;
+            }
+        }
+
+        $crate::serde_impl!($ty, $len $(, $gen: $gent)*);
     }
 }
 
