0f6cfd8659cb31dc67095f5ec4cc2ba6da11c904
Nadav Ivgi <nadav@shesek.info>
2024-06-10 19:51:56 +0300

    Add failing test case for Xpriv set construction

--- bitcoin/src/psbt/mod.rs
+++ bitcoin/src/psbt/mod.rs
@@ -2293,4 +2293,12 @@ mod tests {
         assert_eq!(signing_keys.len(), 1);
         assert_eq!(signing_keys[&0], SigningKeys::Ecdsa(vec![pk]));
     }
+
+    #[test]
+    fn xpriv_sets() {
+        let xpriv = Xpriv::new_master(NetworkKind::Test, &hex!("000102030405060708090a0b0c0d0e0f")).unwrap();
+        Vec::new().push(xpriv.clone());
+        HashSet::new().insert(xpriv.clone());
+        BTreeSet::new().insert(xpriv.clone());
+    }
 }
