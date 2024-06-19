1a2b8645aaa1c905b861c8ba8ecc2606d10c70bf
Tobin C. Harding <me@tobin.cc>
2024-06-19 11:38:21 +1000

    bitcoin: Add comment to manifest
    Add a comment to the manifest features section to try to help the docs
    not go stale again.

--- bitcoin/src/lib.rs
+++ bitcoin/src/lib.rs
@@ -21,7 +21,7 @@
 //! * `default` - enables `std` and `secp-recovery`.
 //! * `ordered` (dependency) - adds implementations of `ArbitraryOrdOrd` to some structs.
 //! * `rand` (transitive dependency) - makes it more convenient to generate random values.
-//! * `rand-std` - same as `rand` but also enables `std` here an in `secp256k1` (and in `rand`).
+//! * `rand-std` - same as `rand` but also enables `std` here and in `secp256k1`.
 //! * `serde` (dependency) - implements `serde`-based serialization and deserialization.
 //! * `secp-lowmemory` - optimizations for low-memory devices.
 //! * `secp-recovery` - enables calculating public key from a signature and message.
