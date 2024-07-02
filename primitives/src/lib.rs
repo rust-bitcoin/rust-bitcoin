65e9ad2fea6064681275551a92816f448e1cde02
Tobin C. Harding <me@tobin.cc>
2024-07-02 13:42:07 +1000

    Introduce empty primitives crate
    Introduce an empty `bitcoin-primitives` crate.
    
    We were give the name on crates.io and previously a version
    `v0.1.16-alpha` was released so we use `v0.100.0`.

---
+++ primitives/src/lib.rs
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: CC0-1.0
+
+//! # Rust Bitcoin - primitive types.
+//!
+//! Primitive data types that are used throughout the [`rust-bitcoin`] ecosystem.
+//!
+//! [`rust-bitcoin`]: <https://github.com/rust-bitcoin>
+
+#![cfg_attr(all(not(test), not(feature = "std")), no_std)]
+// Experimental features we need.
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+// Coding conventions.
+#![warn(missing_docs)]
+// Exclude lints we don't think are valuable.
+#![allow(clippy::needless_question_mark)] // https://github.com/rust-bitcoin/rust-bitcoin/pull/2134
+#![allow(clippy::manual_range_contains)] // More readable than clippy's format.
+#![allow(clippy::needless_borrows_for_generic_args)] // https://github.com/rust-lang/rust-clippy/issues/12454
+
+extern crate alloc;
+
+#[cfg(feature = "std")]
+extern crate std;
