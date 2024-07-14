53c9d193690f5c618eb5bfe185bb0f3918328284
Tobin C. Harding <me@tobin.cc>
2024-07-15 06:40:43 +1000

    Remove bech32 from ParseError
    In #2381 we attempted to fully encapsulate the `bech32` crate to help
    with stabalizing `rust-bitcoin` however we failed to notice the
    `address:ParseError` has a variant that includes `bech32`. Public enums
    have public variant internals in Rust.
    
    However note that we do not actually return the error ever from the code
    because we match on the result of bech32 decoding.
    
    Remove unused error variant from the `address::ParseError` type.
    
    (Note that there are more problems with the parsing because we never
    actually return an error when `bech32` decoding fails, moved that
    problem to a separate issue: #3044.)
    
    Closes: #3043

--- bitcoin/src/address/error.rs
+++ bitcoin/src/address/error.rs
@@ -78,8 +78,6 @@ impl std::error::Error for UnknownAddressTypeError {
 pub enum ParseError {
     /// Base58 error.
     Base58(base58::Error),
-    /// Bech32 segwit decoding error.
-    Bech32(bech32::segwit::DecodeError),
     /// A witness version conversion/parsing error.
     WitnessVersion(witness_version::TryFromError),
     /// A witness program error.
@@ -104,7 +102,6 @@ impl fmt::Display for ParseError {
 
         match *self {
             Base58(ref e) => write_err!(f, "base58 error"; e),
-            Bech32(ref e) => write_err!(f, "bech32 segwit decoding error"; e),
             WitnessVersion(ref e) => write_err!(f, "witness version conversion/parsing error"; e),
             WitnessProgram(ref e) => write_err!(f, "witness program error"; e),
             UnknownHrp(ref e) => write_err!(f, "tried to parse an unknown hrp"; e),
@@ -123,7 +120,6 @@ impl std::error::Error for ParseError {
 
         match *self {
             Base58(ref e) => Some(e),
-            Bech32(ref e) => Some(e),
             WitnessVersion(ref e) => Some(e),
             WitnessProgram(ref e) => Some(e),
             UnknownHrp(ref e) => Some(e),
@@ -137,10 +133,6 @@ impl std::error::Error for ParseError {
 
 impl From<base58::Error> for ParseError {
     fn from(e: base58::Error) -> Self { Self::Base58(e) }
-}
-
-impl From<bech32::segwit::DecodeError> for ParseError {
-    fn from(e: bech32::segwit::DecodeError) -> Self { Self::Bech32(e) }
 }
 
 impl From<witness_version::TryFromError> for ParseError {
