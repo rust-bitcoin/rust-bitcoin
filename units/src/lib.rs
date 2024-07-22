423975a8014521e21a8e671816ceeb65ad60fb27
Tobin C. Harding <me@tobin.cc>
2024-07-22 12:10:28 -0500

    units: Remove serde re-export
    We re-export to help users keep their dependencies in sync but `serde`
    is at `v1.0` so this is not really a problem.
    
    Remove the public re-export of `serde` (and with current MSRV we don't
    need the `extern crate` at all).

--- units/src/lib.rs
+++ units/src/lib.rs
@@ -27,10 +27,6 @@ extern crate alloc;
 #[cfg(feature = "std")]
 extern crate std;
 
-/// A generic serialization/deserialization framework.
-#[cfg(feature = "serde")]
-pub extern crate serde;
-
 pub mod amount;
 #[cfg(feature = "alloc")]
 pub mod block;
