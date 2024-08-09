2e0d89ed117ed59c6c8e957ccec898d8d872285f
Tobin C. Harding <me@tobin.cc>
2024-08-10 07:22:37 +1000

    hashes: Pin in extra_test
    Note:
    
    - The `extra_test.sh` script runs for all toolchains.
    - The `schemars` crate does not use the repo lock files.
    - We need to pin some deps when building the `schemars` test.
    
    Pin in the `extra_test.sh` script, and mention it in the docs so the
    docs don't go stale next MSRV update.
    
    This was previously unnoticed because of the `run_task` bug.
    
    ref: rust-bitcoin/rust-bitcoin-maintainer-tools#10

--- hashes/contrib/extra_tests.sh
+++ hashes/contrib/extra_tests.sh
@@ -5,5 +5,12 @@ set -euox pipefail
 REPO_DIR=$(git rev-parse --show-toplevel)
 
 pushd "$REPO_DIR/hashes/extended_tests/schemars" > /dev/null
+
+# This comment mentions Rust 1.63 to assist grepping when doing MSRV update.
+#
+if cargo --version | grep -q '1\.63'; then
+   cargo update -p regex --precise 1.7.3
+fi
+
 cargo test
 popd > /dev/null
