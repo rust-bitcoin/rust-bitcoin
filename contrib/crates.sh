36a7476ef6f7e4b846c29959070a65451f174f4c
Tobin C. Harding <me@tobin.cc>
2024-08-21 12:57:11 +1000

    Add primitives to the CRATES list
    This should have been done when we introduced the `primitives`
    crate - epic fail.
    
    Intentionally put it before `fuzz` because of the known bug in the CI
    script where nothing after `fuzz` gets tested.

--- contrib/crates.sh
+++ contrib/crates.sh
@@ -5,4 +5,4 @@
 # shellcheck disable=SC2034
 
 # Crates in this workspace to test (note "fuzz" is only built not tested).
-CRATES=("addresses" "base58" "bitcoin" "fuzz" "hashes" "internals" "io" "units")
+CRATES=("addresses" "base58" "bitcoin" "primitives" "fuzz" "hashes" "internals" "io" "units")
