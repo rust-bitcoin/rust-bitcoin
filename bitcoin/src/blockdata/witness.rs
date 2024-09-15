ed138256aa294d8d15f31b837c9c935c913ef652
Shing Him Ng <shinghim@protonmail.com>
2024-09-15 15:07:56 -0500

    Implement Arbitrary for Transaction

--- bitcoin/src/blockdata/witness.rs
+++ bitcoin/src/blockdata/witness.rs
@@ -16,6 +16,8 @@ use crate::crypto::ecdsa;
 use crate::prelude::*;
 use crate::taproot::{self, LeafScript, LeafVersion, TAPROOT_ANNEX_PREFIX, TAPROOT_CONTROL_BASE_SIZE, TAPROOT_LEAF_MASK};
 use crate::{Script, VarInt};
+#[cfg(feature = "arbitrary")]
+use arbitrary::{Arbitrary, Unstructured};
 
 /// The Witness is the data used to unlock bitcoin since the [segwit upgrade].
 ///
@@ -685,6 +687,14 @@ impl From<Vec<&[u8]>> for Witness {
 
 impl Default for Witness {
     fn default() -> Self { Self::new() }
+}
+
+#[cfg(feature = "arbitrary")]
+impl<'a> Arbitrary<'a> for Witness {
+    fn arbitrary(u: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
+        let arbitrary_bytes = Vec::<Vec::<u8>>::arbitrary(u)?;
+        Ok(Witness::from_slice(&arbitrary_bytes))
+    }
 }
 
 #[cfg(test)]
