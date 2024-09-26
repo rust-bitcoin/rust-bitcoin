b6b19bd8735a2602a34ddac24cfce9731766baf4
Tobin C. Harding <me@tobin.cc>
2024-09-26 11:04:23 +1000

    fuzz: Simplify the psbt test

--- fuzz/fuzz_targets/bitcoin/deserialize_psbt.rs
+++ fuzz/fuzz_targets/bitcoin/deserialize_psbt.rs
@@ -18,21 +18,11 @@ fn do_test(data: &[u8]) {
     let psbt: Result<bitcoin::psbt::Psbt, _> = bitcoin::psbt::Psbt::deserialize(bytes);
     match psbt {
         Err(_) => {}
-        Ok(mut psbt) => {
+        Ok(psbt) => {
             let ser = bitcoin::psbt::Psbt::serialize(&psbt);
             let deser = bitcoin::psbt::Psbt::deserialize(&ser).unwrap();
             // Since the fuzz data could order psbt fields differently, we compare to our deser/ser instead of data
             assert_eq!(ser, bitcoin::psbt::Psbt::serialize(&deser));
-
-            let new_bytes = consume_random_bytes(&mut new_data);
-            let psbt_b: Result<bitcoin::psbt::Psbt, _> =
-                bitcoin::psbt::Psbt::deserialize(new_bytes);
-            match psbt_b {
-                Err(_) => {}
-                Ok(mut psbt_b) => {
-                    assert_eq!(psbt_b.combine(psbt.clone()).is_ok(), psbt.combine(psbt_b).is_ok());
-                }
-            }
         }
     }
 }
