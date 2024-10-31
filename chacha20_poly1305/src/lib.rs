9f60e1ae26d2ff29a53e90bccd5a580d7a0f2c84
Tobin C. Harding <me@tobin.cc>
2024-10-31 15:38:01 +1100

    Copy crate level attributes to chacha20_poly1305
    No obvious reason not to; copy the crate level attributes that we have
    in all our other crates.

--- chacha20_poly1305/src/lib.rs
+++ chacha20_poly1305/src/lib.rs
@@ -4,6 +4,16 @@
 //! to form an authenticated encryption with additional data (AEAD) algorithm.
 #![no_std]
 
+// Experimental features we need.
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+// Coding conventions.
+#![warn(missing_docs)]
+#![warn(deprecated_in_future)]
+#![doc(test(attr(warn(unused))))]
+// Exclude lints we don't think are valuable.
+#![allow(clippy::needless_question_mark)] // https://github.com/rust-bitcoin/rust-bitcoin/pull/2134
+#![allow(clippy::manual_range_contains)] // More readable than clippy's format.
+
 #[cfg(feature = "alloc")]
 extern crate alloc;
 #[cfg(feature = "std")]
