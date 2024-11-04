fe8ca21ec282cfe50e3f39b7f86c619d30d7f542
Tobin C. Harding <me@tobin.cc>
2024-11-05 10:44:58 +1100

    hashes: Duplicate impl_from_infallible
    We don't want a dependency on `internals` for `hashes` unless its really
    needed.
    
    Duplicated the `impl_from_infallible` macro into `hashes::error` and
    use it to implement from infallible for `FromSliceError`.

--- hashes/src/error.rs
+++ hashes/src/error.rs
@@ -8,6 +8,8 @@ use core::fmt;
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct FromSliceError(pub(crate) FromSliceErrorInner);
 
+impl_from_infallible!(FromSliceError);
+
 impl FromSliceError {
     /// Returns the expected slice length.
     pub fn expected_length(&self) -> usize { self.0.expected }
@@ -23,6 +25,8 @@ pub(crate) struct FromSliceErrorInner {
     pub(crate) got: usize,
 }
 
+impl_from_infallible!(FromSliceErrorInner);
+
 impl fmt::Display for FromSliceError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "invalid slice length {} (expected {})", self.0.got, self.0.expected)
@@ -31,3 +35,19 @@ impl fmt::Display for FromSliceError {
 
 #[cfg(feature = "std")]
 impl std::error::Error for FromSliceError {}
+
+/// Derives `From<core::convert::Infallible>` for the given type.
+// This is a duplicate of `internals::impl_from_infallible`, see there for complete docs.
+#[doc(hidden)]
+macro_rules! impl_from_infallible {
+    ( $name:ident $(< $( $lt:tt $( : $clt:tt $(+ $dlt:tt )* )? ),+ >)? ) => {
+        impl $(< $( $lt $( : $clt $(+ $dlt )* )? ),+ >)?
+            From<core::convert::Infallible>
+        for $name
+            $(< $( $lt ),+ >)?
+        {
+            fn from(never: core::convert::Infallible) -> Self { match never {} }
+        }
+    }
+}
+pub(crate) use impl_from_infallible;
