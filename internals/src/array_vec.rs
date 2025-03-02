7eefd40ec4b37c44e9fc9c7b9503b48d256d6bae
FT <140458077+zeevick10@users.noreply.github.com>
2025-03-02 22:13:06 +0100

    Update array_vec.rs

--- internals/src/array_vec.rs
+++ internals/src/array_vec.rs
@@ -25,7 +25,7 @@ mod safety_boundary {
         /// Constructs an empty `ArrayVec`.
         pub const fn new() -> Self { Self { len: 0, data: [MaybeUninit::uninit(); CAP] } }
 
-        /// Constructs a new `ArrayVec` initialized with the contets of `slice`.
+        /// Constructs a new `ArrayVec` initialized with the contents of `slice`.
         ///
         /// # Panics
         ///
