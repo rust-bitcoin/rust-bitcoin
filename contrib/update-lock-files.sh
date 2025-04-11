346a4721ea10dc2c77e3e8ac2e921fe20c81356d
Nick Johnson <nick@yonson.dev>
2025-04-11 10:01:52 -0700

    build: use direct-minimal-versions for lockfile
    Adding the unstable direct-minimal-versions flag insteaf of relying on
    just "cargo check" to conservatively upgrade the minimal versions
    lockfile.
    
    The issue with "cargo check" by itselft is that any version downgrades
    of direct dependencies are not automatically reflected in the lockfile.
    They would have to be hand set by developers. The direct-minimal-versions
    flag of cargo does this for us, overriding just that aspect of the
    conservative resolver of check.

--- contrib/update-lock-files.sh
+++ contrib/update-lock-files.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 #
-# Update the minimal/recent lock file
+# Update the minimal and recent lockfiles.
 
 set -euo pipefail
 
-for file in Cargo-minimal.lock Cargo-recent.lock; do
-    cp -f "$file" Cargo.lock
-    cargo check
-    cp -f Cargo.lock "$file"
-done
+
+NIGHTLY=$(cat nightly-version)
+
+# direct-minimal-versions overrides check's already
+# conservative dependecy resolving to force
+# consistent minimal versions for direct dependencies
+# of the workspace. Transitive dependencies are only
+# updated if they must be due to manifest changes.
+cp -f Cargo-minimal.lock Cargo.lock
+cargo +"$NIGHTLY" check -Z direct-minimal-versions
+cp -f Cargo.lock Cargo-minimal.lock
+
+# Conservatively bump of recent dependencies.
+cp -f Cargo-recent.lock Cargo.lock
+cargo check
+cp -f Cargo.lock Cargo-recent.lock
