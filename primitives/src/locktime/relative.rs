7ebf25c17675a480b0f12a8d5b913bc7ffb776a6
Tobin C. Harding <me@tobin.cc>
2025-05-15 09:40:15 +1000

    Modify locktime serde implemenations
    The `units::locktime` types are used for two things:
    
    - They are the inner types of `primitives` `LockTime`s
    - They are used ephemerally for checking satisfaction
    
    Neither of these use cases requires `serde` impls for the `units` types.
    Since we are trying to release 1.0 with minimal amounts of code we
    should remove them.
    
    For `LockTime`s that need to be stored on disk or go over the wire we
    can manually implement the `serde` traits. For `absolute::LockTime` this
    is done already and there is no reason the `relative::LockTime` impl
    cannot be the same [0]. This differs from the current `serde` trait impls
    but we have already decided that in 0.33 we are going to accept breakage
    and direct users to use 0.32 to handle it.
    
    - Remove `serde` stuff from `units::locktime`
    - Manually implement `serde` traits on `relative::LockTime`
    - Fix the regression test to use the new format
    
    While we are at it use a uniform terse call in `serialize`.
    
    [0] This is because there is an unambiguous encoding for the whole set
    of locktimes - consensus encoding.

--- primitives/src/locktime/relative.rs
+++ primitives/src/locktime/relative.rs
@@ -74,7 +74,6 @@ pub type Time = NumberOf512Seconds;
 /// assert!(locktime.is_satisfied_by(current_height, current_mtp, utxo_height, utxo_mtp));
 /// ```
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum LockTime {
     /// A block height lock time value.
     Blocks(NumberOfBlocks),
@@ -430,6 +429,27 @@ impl convert::TryFrom<Sequence> for LockTime {
 impl From<LockTime> for Sequence {
     #[inline]
     fn from(lt: LockTime) -> Sequence { lt.to_sequence() }
+}
+
+#[cfg(feature = "serde")]
+impl serde::Serialize for LockTime {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.to_consensus_u32().serialize(serializer)
+    }
+}
+
+#[cfg(feature = "serde")]
+impl<'de> serde::Deserialize<'de> for LockTime {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        u32::deserialize(deserializer)
+            .and_then(|n| Self::from_consensus(n).map_err(serde::de::Error::custom))
+    }
 }
 
 /// Error returned when a sequence number is parsed as a lock time, but its
