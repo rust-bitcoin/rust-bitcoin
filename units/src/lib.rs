7b4c5dd33ff226aed1acc96dfe9b73456dac0d48
Martin Habovstiak <martin.habovstiak@gmail.com>
2025-07-03 12:50:05 +0200

    Improve the documentation of `units`
    The documentation was very short and didn't explain the purpose of the
    crate, its relation to `bitcoin`, standard topics like features or
    MSRV... This change extends the documentation to explain these things.

--- units/src/lib.rs
+++ units/src/lib.rs
@@ -1,12 +1,43 @@
 // SPDX-License-Identifier: CC0-1.0
 
-//! # Rust Bitcoin Unit Types
+//! Basic (numeric) types used by the Rust Bitcoin ecosystem.
 //!
-//! This library provides basic types used by the Rust Bitcoin ecosystem.
+//! This crate contains basic types that have minimal requirements on the platform they run on.
+//! Specifically, they do not require an allocator and they do not require `usize` to be at least
+//! 32-bit. If you need more than this crate provides check the [`bitcoin`] crate or the
+//! [`bitcoin-primitives`] crate.
 //!
-//! If you are using `rust-bitcoin` then you do not need to access this crate directly. Everything
-//! here is re-exported in `rust-bitcoin` at the same path. Also the same re-exports exist in
-//! `primitives` if you are using that crate instead of `bitcoin`.
+//! # Guidance on crate use
+//!
+//! *If you are using the `bitcoin` crate then you do not need to access this crate directly.*
+//!
+//! Everything here is re-exported in `bitcoin` at the same path. Also the same re-exports exist in
+//! `bitcoin-primitives` if you are using that crate instead of `bitcoin`.
+//!
+//! Libraries that only need the types present in this crate should depend only on this crate, not
+//! `bitcoin` or `bitcoin-primitives` so that they don't add bloat to compilation and review. It is
+//! recommended that binaries or other root crates depend on `bitcoin` during the prototyping stage
+//! and then optionally try to trim down the dependencies by using the leaf crates. However this is
+//! unlikely to be feasible for non-trivial applications.
+//!
+//! ## Features
+//!
+//! * `std` - turns on `std` integration, mainly the `std::error::Error` trait in old Rust versions.
+//! * `alloc` - turns on features that require the `alloc` crate, such as `String` interop.
+//! * `serde` - causes the crate to depend on `serde` and provide support for serializing and
+//!   deserializing its types.
+//! * `arbitrary` - causes the crate to depend on `arbitrary` and implement the `Arbitrary` trait.
+//!
+//! # MSRV
+//!
+//! This crate supports Rust 1.74, however some of its dependencies may not do so or may require
+//! pinning. Similarly, some features may require newer Rust version (implicitly or explicitly).
+//!
+//! ## Policy
+//!
+//! Our MSRV policy it to only bump MSRV to the one that is available on the latest Debian stable
+//! and is at least two years old. However, we will try to be even more conservative when practical
+//! given this crate is very widely used.
 //!
 //! # Examples
 //!
@@ -18,6 +49,9 @@
 //! # let _ = amount;
 //! # Ok::<_, amount::OutOfRangeError>(())
 //! ```
+//!
+//! [`bitcoin`]: https://docs.rs/bitcoin
+//! [`bitcoin-primitives`]: https://docs.rs/bitcoin-primitives
 
 #![no_std]
 // Coding conventions.
