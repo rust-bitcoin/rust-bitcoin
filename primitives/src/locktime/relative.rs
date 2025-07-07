29a6fe293a7c6f36f3bc15700390c279bb7832e0
Tobin C. Harding <me@tobin.cc>
2025-07-07 12:58:32 +1000

    Remove links to TxIn from locktime docs
    In preparation for moving the locktime code to `units` remove all
    links to `TxIn` from the rustdocs of the `relative` module.

--- primitives/src/locktime/relative.rs
+++ primitives/src/locktime/relative.rs
@@ -10,8 +10,9 @@ use core::{convert, fmt};
 use internals::write_err;
 
 use crate::Sequence;
+
 #[cfg(all(doc, feature = "alloc"))]
-use crate::{relative, TxIn};
+use crate::relative;
 
 #[rustfmt::skip]                // Keep public re-exports separate.
 #[doc(inline)]
@@ -28,8 +29,8 @@ pub type Time = NumberOf512Seconds;
 
 /// A relative lock time value, representing either a block height or time (512 second intervals).
 ///
-/// Used for sequence numbers (`nSequence` in Bitcoin Core and [`TxIn::sequence`]
-/// in this library) and also for the argument to opcode `OP_CHECKSEQUENCEVERIFY`.
+/// Used for sequence numbers (`nSequence` in Bitcoin Core and `TxIn::sequence`
+/// in `rust-bitcoin`) and also for the argument to opcode `OP_CHECKSEQUENCEVERIFY`.
 ///
 /// # Note on ordering
 ///
