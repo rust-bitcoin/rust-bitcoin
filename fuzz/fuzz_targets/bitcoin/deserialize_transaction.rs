74ff6f5ff5eb4c9454bf6d4f5ae6e505cf11caa8
Shing Him Ng <shinghim@protonmail.com>
2025-07-21 20:37:21 -0500

    Update `deserialize_transaction` fuzz target to use `Arbitrary` impl

--- fuzz/fuzz_targets/bitcoin/deserialize_transaction.rs
+++ fuzz/fuzz_targets/bitcoin/deserialize_transaction.rs
@@ -1,29 +1,34 @@
-use bitcoin::transaction::TransactionExt as _;
+use arbitrary::{Arbitrary, Unstructured};
 use honggfuzz::fuzz;
+use bitcoin::consensus::{deserialize, serialize};
+use bitcoin::Transaction;
+use bitcoin::transaction::TransactionExt as _;
 
 fn do_test(data: &[u8]) {
-    let tx_result: Result<bitcoin::transaction::Transaction, _> =
-        bitcoin::consensus::encode::deserialize(data);
-    match tx_result {
-        Err(_) => {}
-        Ok(mut tx) => {
-            let ser = bitcoin::consensus::encode::serialize(&tx);
-            assert_eq!(&ser[..], data);
-            let len = ser.len();
-            let calculated_weight = tx.weight().to_wu() as usize;
-            for input in &mut tx.input {
-                input.witness = bitcoin::witness::Witness::default();
-            }
-            let no_witness_len = bitcoin::consensus::encode::serialize(&tx).len();
-            // For 0-input transactions, `no_witness_len` will be incorrect because
-            // we serialize as SegWit even after "stripping the witnesses". We need
-            // to drop two bytes (i.e. eight weight). Similarly, calculated_weight is
-            // incorrect and needs 2 wu removing for the marker/flag bytes.
-            if tx.input.is_empty() {
-                assert_eq!(no_witness_len * 3 + len - 8, calculated_weight - 2);
-            } else {
-                assert_eq!(no_witness_len * 3 + len, calculated_weight);
-            }
+    let mut u = Unstructured::new(data);
+    let t = Transaction::arbitrary(&mut u);
+
+    if let Ok(mut tx) = t {
+        let serialized = serialize(&tx);
+        let deserialized: Result<Transaction, _> = deserialize(serialized.as_slice());
+
+        assert!(deserialized.is_ok(), "Fuzz error: {:?}", deserialized.err().unwrap());
+        assert_eq!(deserialized.unwrap(), tx);
+
+        let len = serialized.len();
+        let calculated_weight = tx.weight().to_wu() as usize;
+        for input in &mut tx.input {
+            input.witness = bitcoin::witness::Witness::default();
+        }
+        let no_witness_len = bitcoin::consensus::encode::serialize(&tx).len();
+        // For 0-input transactions, `no_witness_len` will be incorrect because
+        // we serialize as SegWit even after "stripping the witnesses". We need
+        // to drop two bytes (i.e. eight weight). Similarly, calculated_weight is
+        // incorrect and needs 2 wu removing for the marker/flag bytes.
+        if tx.input.is_empty() {
+            assert_eq!(no_witness_len * 3 + len - 8, calculated_weight - 2);
+        } else {
+            assert_eq!(no_witness_len * 3 + len, calculated_weight);
         }
     }
 }
