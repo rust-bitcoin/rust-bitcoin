e7985c29d6111946c183a95ca98059a65811638d
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-08-23 14:11:43 +0000

    consensus_encoding: introduce tuple encoders
    Most Bitcoin objects are composites of 2 or 4 smaller Bitcoin objects.
    In the case of block::Header there are 6. Support these by implementing
    a basic 2-subset state machine and composing it a bunch of times.
    
    This commit is a little bit noisy but shouldn't be bad.

--- consensus_encoding/src/encode/encoders.rs
+++ consensus_encoding/src/encode/encoders.rs
@@ -55,3 +55,99 @@ impl<'e, const N: usize> Encoder<'e> for ArrayEncoder<N> {
         false
     }
 }
+
+/// An encoder which encodes two objects, one after the other.
+pub struct Encoder2<A, B> {
+    enc_idx: usize,
+    enc_1: A,
+    enc_2: B,
+}
+
+impl<A, B> Encoder2<A, B> {
+    /// Constructs a new composite encoder.
+    pub fn new(enc_1: A, enc_2: B) -> Self { Self { enc_idx: 0, enc_1, enc_2 } }
+}
+
+impl<'e, A: Encoder<'e>, B: Encoder<'e>> Encoder<'e> for Encoder2<A, B> {
+    fn current_chunk(&self) -> Option<&[u8]> {
+        if self.enc_idx == 0 {
+            self.enc_1.current_chunk()
+        } else {
+            self.enc_2.current_chunk()
+        }
+    }
+
+    fn advance(&mut self) -> bool {
+        if self.enc_idx == 0 {
+            if !self.enc_1.advance() {
+                self.enc_idx += 1;
+            }
+            true
+        } else {
+            self.enc_2.advance()
+        }
+    }
+}
+
+// For now we implement every higher encoder by composing Encoder2s, because
+// I'm lazy and this is trivial both to write and to review. For efficiency, we
+// should eventually unroll all of these. There are only a couple of them. The
+// unrolled versions should be macro-izable, if we want to do that.
+
+/// An encoder which encodes three objects, one after the other.
+pub struct Encoder3<A, B, C> {
+    inner: Encoder2<Encoder2<A, B>, C>,
+}
+
+impl<A, B, C> Encoder3<A, B, C> {
+    /// Constructs a new composite encoder.
+    pub fn new(enc_1: A, enc_2: B, enc_3: C) -> Self {
+        Self { inner: Encoder2::new(Encoder2::new(enc_1, enc_2), enc_3) }
+    }
+}
+
+impl<'e, A: Encoder<'e>, B: Encoder<'e>, C: Encoder<'e>> Encoder<'e> for Encoder3<A, B, C> {
+    fn current_chunk(&self) -> Option<&[u8]> { self.inner.current_chunk() }
+    fn advance(&mut self) -> bool { self.inner.advance() }
+}
+
+/// An encoder which encodes four objects, one after the other.
+pub struct Encoder4<A, B, C, D> {
+    inner: Encoder2<Encoder2<A, B>, Encoder2<C, D>>,
+}
+
+impl<A, B, C, D> Encoder4<A, B, C, D> {
+    /// Constructs a new composite encoder.
+    pub fn new(enc_1: A, enc_2: B, enc_3: C, enc_4: D) -> Self {
+        Self { inner: Encoder2::new(Encoder2::new(enc_1, enc_2), Encoder2::new(enc_3, enc_4)) }
+    }
+}
+
+impl<'e, A: Encoder<'e>, B: Encoder<'e>, C: Encoder<'e>, D: Encoder<'e>> Encoder<'e> for Encoder4<A, B, C, D> {
+    fn current_chunk(&self) -> Option<&[u8]> { self.inner.current_chunk() }
+    fn advance(&mut self) -> bool { self.inner.advance() }
+}
+
+/// An encoder which encodes six objects, one after the other.
+pub struct Encoder6<A, B, C, D, E, F> {
+    inner: Encoder2<Encoder3<A, B, C>, Encoder3<D, E, F>>,
+}
+
+impl<A, B, C, D, E, F> Encoder6<A, B, C, D, E, F> {
+    /// Constructs a new composite encoder.
+    pub fn new(enc_1: A, enc_2: B, enc_3: C, enc_4: D, enc_5: E, enc_6: F) -> Self {
+        Self {
+            inner: Encoder2::new(
+                Encoder3::new(enc_1, enc_2, enc_3),
+                Encoder3::new(enc_4, enc_5, enc_6),
+            ),
+        }
+    }
+}
+
+impl<'e, A: Encoder<'e>, B: Encoder<'e>, C: Encoder<'e>, D: Encoder<'e>, E: Encoder<'e>, F: Encoder<'e>> Encoder<'e>
+    for Encoder6<A, B, C, D, E, F>
+{
+    fn current_chunk(&self) -> Option<&[u8]> { self.inner.current_chunk() }
+    fn advance(&mut self) -> bool { self.inner.advance() }
+}
