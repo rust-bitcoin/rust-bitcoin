b2164e2e213d6bf310fc4a1732eae129c33fae6f
Tobin C. Harding <me@tobin.cc>
2025-09-04 13:53:23 +1000

    Add lockfile
    These are not used by CI but are hopefully useful for testing with
    Nix.
    
    The CI here is old as hell. Just to get things working create a lock
    file, copy it into place, and the use `cargo --locked` anyplace that
    will be run by the MSRV job so it doesn't get updated.

--- contrib/test.sh
+++ contrib/test.sh
@@ -2,7 +2,12 @@
 
 set -ex
 
+REPO_DIR=$(git rev-parse --show-toplevel)
 CRATES="bitcoin hashes internals"
+
+# All `cargo` invocations excl. ones that are run by setting environment
+# variables (eg DO_FMT) use `--locked` so as not to update this.
+cp "$REPO_DIR/Cargo-recent.lock" "$REPO_DIR/Cargo.lock"
 
 for crate in ${CRATES}
 do
