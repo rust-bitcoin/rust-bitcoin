a1d0de8ad382e74be4b5144500e878b63ed85003
Tobin C. Harding <me@tobin.cc>
2025-11-18 13:23:32 +1100

    Move/add From<PushBytes> for SerializedSignature
    Move the `ecdsa` impl to `script::push_bytes` and add one for the
    `taproot` type.

--- bitcoin/src/blockdata/script/push_bytes.rs
+++ bitcoin/src/blockdata/script/push_bytes.rs
@@ -4,6 +4,7 @@
 
 use core::ops::{Deref, DerefMut};
 
+use crate::crypto::{ecdsa, taproot};
 use crate::prelude::{Borrow, BorrowMut};
 use crate::script;
 
@@ -383,6 +384,20 @@ impl Borrow<PushBytes> for PushBytesBuf {
 
 impl BorrowMut<PushBytes> for PushBytesBuf {
     fn borrow_mut(&mut self) -> &mut PushBytes { self.as_mut_push_bytes() }
+}
+
+impl AsRef<PushBytes> for ecdsa::SerializedSignature {
+    #[inline]
+    fn as_ref(&self) -> &PushBytes {
+        <&PushBytes>::try_from(<Self as AsRef<[u8]>>::as_ref(self)).expect("max length 73 bytes is valid")
+    }
+}
+
+impl AsRef<PushBytes> for taproot::SerializedSignature {
+    #[inline]
+    fn as_ref(&self) -> &PushBytes {
+        <&PushBytes>::try_from(<Self as AsRef<[u8]>>::as_ref(self)).expect("max length 65 bytes is valid") 
+    }
 }
 
 /// Reports information about failed conversion into `PushBytes`.
