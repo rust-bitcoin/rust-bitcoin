d14e4f07fce9ce65c274a29e104444d257385fa3
yancy <github@yancy.lol>
2026-01-07 17:19:58 -0600

    p2p: implement decoding for Address
    The decoder decodes a slice `&[u8]`  however address is of type
    `&[u16; 8]`.  To make the conversion, chunk the u8 slice into groups of
    two, then, for each `[u8, u8]` pair, convert to a `u16` with
    `try_into()`.  `TryInto` should be infalliable here since the decode of
    16 bytes was succfull already using `ArrayDecoder<16>`.
    
    Endian byte order for both IPv4 and the port number are interpreted as
    big endian per the Addr section of:
    https://developer.bitcoin.org/reference/p2p_networking.html
    
    In the test cases, `deserialize` is swapped for
    `encoding::decode_from_slice` since the addition of push bytes decoding
    is planned to replace the existing `deserialize` implementation.

--- p2p/src/address.rs
+++ p2p/src/address.rs
@@ -138,6 +138,72 @@ impl ToSocketAddrs for Address {
     }
 }
 
+type AddressInnerDecoder = encoding::Decoder3<
+    crate::ServiceFlagsDecoder,
+    encoding::ArrayDecoder<16>,
+    encoding::ArrayDecoder<2>,
+>;
+
+/// The Decoder for [`Address`].
+pub struct AddressDecoder(AddressInnerDecoder);
+
+impl encoding::Decoder for AddressDecoder {
+    type Output = Address;
+    type Error = AddressDecoderError;
+
+    #[inline]
+    fn push_bytes(&mut self, bytes: &mut &[u8]) -> Result<bool, Self::Error> {
+        self.0.push_bytes(bytes).map_err(AddressDecoderError)
+    }
+
+    #[inline]
+    fn end(self) -> Result<Self::Output, Self::Error> {
+        let (services, raw_address, port) = self.0.end().map_err(AddressDecoderError)?;
+        let mut address: [u16; 8] = [0; 8];
+
+        for (index, value) in raw_address.chunks(2).enumerate() {
+            let arr: [u8; 2] =
+                <&[u8] as TryInto<[u8; 2]>>::try_into(value).expect("16 bytes decoded");
+            address[index] = u16::from_be_bytes(arr);
+        }
+
+        Ok(Address { services, address, port: u16::from_be_bytes(port) })
+    }
+
+    #[inline]
+    fn read_limit(&self) -> usize { self.0.read_limit() }
+}
+
+impl encoding::Decodable for Address {
+    type Decoder = AddressDecoder;
+    fn decoder() -> Self::Decoder {
+        AddressDecoder(encoding::Decoder3::new(
+            ServiceFlags::decoder(),
+            encoding::ArrayDecoder::<16>::new(),
+            encoding::ArrayDecoder::<2>::new(),
+        ))
+    }
+}
+
+/// An error consensus decoding a [`AddressDecoderError`].
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct AddressDecoderError(<AddressInnerDecoder as encoding::Decoder>::Error);
+
+impl From<Infallible> for AddressDecoderError {
+    fn from(never: Infallible) -> Self { match never {} }
+}
+
+impl fmt::Display for AddressDecoderError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        internals::write_err!(f, "address decoder error"; self.0)
+    }
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for AddressDecoderError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> { Some(&self.0) }
+}
+
 /// Supported networks for use in BIP-0155 addrv2 message
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
 pub enum AddrV2 {
@@ -912,7 +978,7 @@ mod test {
 
     #[test]
     fn deserialize_address() {
-        let mut addr: Result<Address, _> = deserialize(&[
+        let mut addr: Result<Address, _> = encoding::decode_from_slice(&[
             1u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff, 0x0a, 0, 0, 1,
             0x20, 0x8d,
         ]);
@@ -923,7 +989,7 @@ mod test {
         assert_eq!(full.address, [0, 0, 0, 0, 0, 0xffff, 0x0a00, 0x0001]);
         assert_eq!(full.port, 8333);
 
-        addr = deserialize(&[
+        addr = encoding::decode_from_slice(&[
             1u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff, 0x0a, 0, 0, 1,
         ]);
         assert!(addr.is_err());
