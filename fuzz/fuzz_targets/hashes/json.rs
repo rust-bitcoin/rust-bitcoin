17eccb3d1e8a2e34a2e71119312010f09306ac45
Shing Him Ng <shinghim@protonmail.com>
2026-01-08 20:37:13 -0600

    Migrate from Honggfuzz to libfuzzer
    This commit has a few main parts:
    
    - Update CI to install required dependencies for libfuzzer
    The default Cross s390x image uses an older version of GCC (Ubuntu
    20.04), which lacks std::clamp needed by libfuzzer-sys. The s390x
    Dockerfile provides Cross with a container with GCC 13, native build
    tools, and QEMU for running s390x tests. Cross.toml configures Cross to
    use this custom image instead of the default.
    
    - Update the lock files
    - Update the fuzz targets to use libfuzzer
    - Update the fuzz helper scripts and documentation
    - Update the duplicate dependency whitelist to include libc
    - Move `fuzz` into a nested workspace

--- fuzz/fuzz_targets/hashes/json.rs
+++ fuzz/fuzz_targets/hashes/json.rs
@@ -1,6 +1,10 @@
-use bitcoin::hashes::{ripemd160, sha1, sha256d, sha512, Hmac};
-use honggfuzz::fuzz;
+#![cfg_attr(fuzzing, no_main)]
+#![cfg_attr(not(fuzzing), allow(unused))]
+
+use libfuzzer_sys::fuzz_target;
 use serde::{Deserialize, Serialize};
+
+use bitcoin::hashes::{ripemd160, sha1, sha256d, sha512, Hmac};
 
 #[derive(Deserialize, Serialize)]
 struct Hmacs {
@@ -15,6 +19,9 @@ struct Main {
     sha2d: sha256d::Hash,
 }
 
+#[cfg(not(fuzzing))]
+fn main() {}
+
 fn do_test(data: &[u8]) {
     if let Ok(m) = serde_json::from_slice::<Main>(data) {
         let vec = serde_json::to_vec(&m).unwrap();
@@ -22,35 +29,6 @@ fn do_test(data: &[u8]) {
     }
 }
 
-fn main() {
-    loop {
-        fuzz!(|d| { do_test(d) });
-    }
-}
-
-#[cfg(all(test, fuzzing))]
-mod tests {
-    fn extend_vec_from_hex(hex: &str, out: &mut Vec<u8>) {
-        let mut b = 0;
-        for (idx, c) in hex.as_bytes().iter().enumerate() {
-            b <<= 4;
-            match *c {
-                b'A'..=b'F' => b |= c - b'A' + 10,
-                b'a'..=b'f' => b |= c - b'a' + 10,
-                b'0'..=b'9' => b |= c - b'0',
-                _ => panic!("Bad hex"),
-            }
-            if (idx & 1) == 1 {
-                out.push(b);
-                b = 0;
-            }
-        }
-    }
-
-    #[test]
-    fn duplicate_crash() {
-        let mut a = Vec::new();
-        extend_vec_from_hex("00000", &mut a);
-        super::do_test(&a);
-    }
-}
+fuzz_target!(|data| {
+    do_test(data)
+});
--- fuzz/fuzz_targets/units/standard_checks.rs
+++ fuzz/fuzz_targets/units/standard_checks.rs
@@ -1,10 +1,13 @@
-use bitcoin::absolute::{Height, MedianTimePast};
-use bitcoin::relative::{NumberOf512Seconds, NumberOfBlocks};
+#![cfg_attr(fuzzing, no_main)]
+#![cfg_attr(not(fuzzing), allow(unused))]
+
+use libfuzzer_sys::fuzz_target;
 use bitcoin::{
     Amount, BlockHeight, BlockHeightInterval, BlockMtp, BlockMtpInterval, BlockTime, FeeRate,
     Sequence, SignedAmount, Weight,
 };
-use honggfuzz::fuzz;
+use bitcoin::absolute::{Height, MedianTimePast};
+use bitcoin::relative::{NumberOf512Seconds, NumberOfBlocks};
 use standard_test::StandardChecks as _;
 
 /// Implements the traits on the wrapper type $ty. Intended only to be called from inside wrap_for_checks!
@@ -68,6 +71,9 @@ mod fuzz {
     wrap_for_checks!(Weight, super::Weight::MIN_TRANSACTION);
 }
 
+#[cfg(not(fuzzing))]
+fn main() {}
+
 fn do_test(data: &[u8]) {
     fuzz::Amount::one_iteration(data);
     fuzz::BlockHeight::one_iteration(data);
@@ -85,37 +91,6 @@ fn do_test(data: &[u8]) {
     fuzz::Weight::one_iteration(data);
 }
 
-fn main() {
-    loop {
-        fuzz!(|data| {
-            do_test(data);
-        });
-    }
-}
-
-#[cfg(all(test, fuzzing))]
-mod tests {
-    fn extend_vec_from_hex(hex: &str, out: &mut Vec<u8>) {
-        let mut b = 0;
-        for (idx, c) in hex.as_bytes().iter().enumerate() {
-            b <<= 4;
-            match *c {
-                b'A'..=b'F' => b |= c - b'A' + 10,
-                b'a'..=b'f' => b |= c - b'a' + 10,
-                b'0'..=b'9' => b |= c - b'0',
-                _ => panic!("Bad hex"),
-            }
-            if (idx & 1) == 1 {
-                out.push(b);
-                b = 0;
-            }
-        }
-    }
-
-    #[test]
-    fn duplicate_crash() {
-        let mut a = Vec::new();
-        extend_vec_from_hex("00000000", &mut a);
-        super::do_test(&a);
-    }
-}
+fuzz_target!(|data| {
+    do_test(data);
+});
