3c19767f5a843fbf701dc914ba6e3406e931bbcc
yancy <github@yancy.lol>
2026-01-13 15:10:13 -0600

    p2p: implement encoding for Address
    port and address are both converted to an array type &[u8] as required
    by `ArrayEncoder` using `to_be_bytes()`.  Big endian byte order is
    specified in:
    https://developer.bitcoin.org/reference/p2p_networking.html
    
    Since address is an array of `[u16; 8]`, each respective `u16` is split
    into a `[u8; 2]`, then both elements are filled into the resulting
    `[u8; 32]`.
    
    In the test cases, replace `serialize` with `encoding::encode_to_vec`
    since the push encoding here is planned to replace the existing
    `serialize` implementation.

--- p2p/src/address.rs
+++ p2p/src/address.rs
@@ -154,6 +154,39 @@ impl ToSocketAddrs for Address {
     }
 }
 
+encoding::encoder_newtype! {
+    /// The encoder for the [`Address`] type.
+    pub struct AddressEncoder<'e>(encoding::Encoder3<
+        crate::ServiceFlagsEncoder<'e>,
+        encoding::ArrayEncoder<16>,
+        encoding::ArrayEncoder<2>
+    >);
+}
+
+impl encoding::Encodable for Address {
+    type Encoder<'e>
+        = AddressEncoder<'e>
+    where
+        Self: 'e;
+
+    fn encoder(&self) -> Self::Encoder<'_> {
+        let mut address: [u8; 16] = [0; 16];
+        for (index, value) in self.address.iter().enumerate() {
+            let arr: [u8; 2] = value.to_be_bytes();
+            address[index * 2] = arr[0];
+            address[index * 2 + 1] = arr[1];
+        }
+
+        let enc = encoding::Encoder3::new(
+            self.services.encoder(),
+            encoding::ArrayEncoder::without_length_prefix(address),
+            encoding::ArrayEncoder::without_length_prefix(self.port.to_be_bytes()),
+        );
+
+        AddressEncoder::new(enc)
+    }
+}
+
 type AddressInnerDecoder = encoding::Decoder3<
     crate::ServiceFlagsDecoder,
     encoding::ArrayDecoder<16>,
@@ -947,7 +980,7 @@ mod test {
     #[test]
     fn serialize_address() {
         assert_eq!(
-            serialize(&Address {
+            encoding::encode_to_vec(&Address {
                 services: ServiceFlags::NETWORK,
                 address: [0, 0, 0, 0, 0, 0xffff, 0x0a00, 0x0001],
                 port: 8333
