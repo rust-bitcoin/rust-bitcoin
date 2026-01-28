676907c6af261e5deac17a886a1a7173c7ce5418
Mitchell Bagot <packof14eggies@gmail.com>
2026-01-28 12:59:08 +1100

    Fix Error source for ParseDenominationError
    The ParseDenominationError return None for the error source in the
    std::error::Error trait implementation. Since the type is a wrapper
    enum for other errors, it should return the inner error in source().
    
    Return the inner error types for ParseDenominationError in source().

--- units/src/amount/error.rs
+++ units/src/amount/error.rs
@@ -385,7 +385,8 @@ impl fmt::Display for ParseDenominationError {
 impl std::error::Error for ParseDenominationError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match *self {
-            Self::Unknown(_) | Self::PossiblyConfusing(_) => None,
+            Self::Unknown(ref e) => Some(e),
+            Self::PossiblyConfusing(ref e) => Some(e),
         }
     }
 }
