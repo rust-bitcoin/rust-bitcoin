9fedc40bb0b0285ae41f9f200b529acb0be5c589
Nick Johnson <nick@yonson.dev>
2026-01-30 12:10:45 -0800

    addresses,bip158,crypto: migrate lint settings

--- addresses/src/lib.rs
+++ addresses/src/lib.rs
@@ -17,10 +17,6 @@
 // Coding conventions.
 #![warn(deprecated_in_future)]
 #![warn(missing_docs)]
-// Exclude lints we don't think are valuable.
-#![allow(clippy::needless_question_mark)] // https://github.com/rust-bitcoin/rust-bitcoin/pull/2134
-#![allow(clippy::manual_range_contains)] // More readable than clippy's format.
-#![allow(clippy::uninlined_format_args)] // Allow `format!("{}", x)` instead of enforcing `format!("{x}")`
 
 extern crate alloc;
 
