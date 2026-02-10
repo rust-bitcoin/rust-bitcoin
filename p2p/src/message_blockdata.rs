49c3d06257531c3d1c6e756ba8ad28f6bfc23648
Nick Johnson <nick@yonson.dev>
2026-02-10 09:29:10 -0800

    Drop the hex_lit dep in favor of built in hex

--- p2p/src/message_blockdata.rs
+++ p2p/src/message_blockdata.rs
@@ -432,7 +432,7 @@ impl<'a> Arbitrary<'a> for Inventory {
 #[cfg(test)]
 mod tests {
     use bitcoin::consensus::encode::{deserialize, serialize};
-    use hex_lit::hex;
+    use hex::hex;
 
     use super::*;
 
