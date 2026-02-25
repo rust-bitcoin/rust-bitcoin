125f23f8465cf72f56f766896da81fc38b22da19
Mitchell Bagot <packof14eggies@gmail.com>
2026-02-25 14:17:39 +1100

    Add to_secret_bytes to PrivateKey
    Private keys have statically-known length of 32 bytes, unlike public
    keys. To simplify roundtrips with from_byte_array and secp equivalents,
    a function to return the fixed length array should be present.
    
    Introduce to_secret_bytes function to PrivateKey. Replace to_vec range
    implementation with to_secret_bytes().to_vec().

--- bitcoin/src/crypto/key.rs
+++ bitcoin/src/crypto/key.rs
@@ -859,7 +859,12 @@ impl PrivateKey {
     pub fn to_bytes(self) -> Vec<u8> { self.to_vec() }
 
     /// Serializes the private key to bytes.
-    pub fn to_vec(self) -> Vec<u8> { self.as_inner()[..].to_vec() }
+    pub fn to_vec(self) -> Vec<u8> { self.to_secret_bytes().to_vec() }
+
+    /// Serializes the private key to bytes.
+    pub fn to_secret_bytes(self) -> [u8; 32] {
+        self.as_inner().to_secret_bytes()
+    }
 
     /// Deserializes a private key from a byte array.
     pub fn from_byte_array(
