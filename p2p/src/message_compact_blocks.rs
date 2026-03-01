cc2088dc5eb5ada969ba86b1a46c9c942620a4df
yancy <github@yancy.lol>
2026-03-01 06:19:17 -0600

    p2p: add Sendcmpct Decoder
    sendcmpct is the first message sent after the handshake is complete,
    therefore, this is the next in line for implementation after
    VersionMessage.  Note that verack has no body and is a header-only
    message, therefore does not require a message body decoder/encoder.
    
    To implement the Decoder, I follow the section sendcmpct in:
    https://en.bitcoin.it/wiki/Protocol_documentation.  The docs define the
    two fields as a 1 byte boolean field followed by an 8 byte integer.

--- p2p/src/message_compact_blocks.rs
+++ p2p/src/message_compact_blocks.rs
@@ -3,6 +3,9 @@
 //!
 //! BIP-0152  Compact Blocks network messages
 
+use core::convert::Infallible;
+use core::fmt;
+
 #[cfg(feature = "arbitrary")]
 use arbitrary::{Arbitrary, Unstructured};
 
@@ -17,6 +20,61 @@ pub struct SendCmpct {
     pub version: u64,
 }
 impl_consensus_encoding!(SendCmpct, send_compact, version);
+
+type SendCmpctInnerDecoder =
+    encoding::Decoder2<encoding::ArrayDecoder<1>, encoding::ArrayDecoder<8>>;
+
+/// The Decoder for [`SendCmpct`].
+pub struct SendCmpctDecoder(SendCmpctInnerDecoder);
+
+impl encoding::Decoder for SendCmpctDecoder {
+    type Output = SendCmpct;
+    type Error = SendCmpctDecoderError;
+
+    #[inline]
+    fn push_bytes(&mut self, bytes: &mut &[u8]) -> Result<bool, Self::Error> {
+        self.0.push_bytes(bytes).map_err(SendCmpctDecoderError)
+    }
+
+    #[inline]
+    fn end(self) -> Result<Self::Output, Self::Error> {
+        let (send_compact, version) = self.0.end().map_err(SendCmpctDecoderError)?;
+        let send_compact = send_compact[0] != 0;
+        let version = u64::from_le_bytes(version);
+        Ok(SendCmpct { send_compact, version })
+    }
+
+    #[inline]
+    fn read_limit(&self) -> usize { self.0.read_limit() }
+}
+
+impl encoding::Decodable for SendCmpct {
+    type Decoder = SendCmpctDecoder;
+    fn decoder() -> Self::Decoder {
+        SendCmpctDecoder(encoding::Decoder2::new(
+            encoding::ArrayDecoder::<1>::new(),
+            encoding::ArrayDecoder::<8>::new(),
+        ))
+    }
+}
+
+/// An error consensus decoding a [`SendCmpctDecoderError`].
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SendCmpctDecoderError(<SendCmpctInnerDecoder as encoding::Decoder>::Error);
+impl From<Infallible> for SendCmpctDecoderError {
+    fn from(never: Infallible) -> Self { match never {} }
+}
+
+impl fmt::Display for SendCmpctDecoderError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        internals::write_err!(f, "address decoder error"; self.0)
+    }
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for SendCmpctDecoderError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> { Some(&self.0) }
+}
 
 #[cfg(feature = "arbitrary")]
 impl<'a> Arbitrary<'a> for SendCmpct {
