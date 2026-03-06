24c8e2590d8a6c25a312fb778641518c08bb1310
busayo-OD <toyinoluwabusayo@gmail.com>
2026-03-06 23:02:13 +0100

    primitives: add encoder/decoder types to api.rs

--- primitives/tests/api.rs
+++ primitives/tests/api.rs
@@ -265,6 +265,37 @@ fn api_can_use_all_types_from_module_script() {
         RedeemScriptSizeError, ScriptHash, ScriptPubKey, ScriptPubKeyBuf, ScriptSig, ScriptSigBuf,
         WScriptHash, WitnessScriptSizeError,
     };
+    use bitcoin_primitives::script::{ScriptBufDecoder, ScriptBufDecoderError, ScriptEncoder};
+}
+
+#[test]
+fn api_can_use_all_types_from_module_block() {
+    use bitcoin_primitives::block::{
+        BlockDecoder, BlockDecoderError, BlockEncoder, BlockHashDecoder, BlockHashDecoderError,
+        BlockHashEncoder, HeaderDecoder, HeaderEncoder, VersionDecoder, VersionDecoderError,
+        VersionEncoder,
+    };
+}
+
+#[test]
+fn api_can_use_all_types_from_module_merkle_tree() {
+    use bitcoin_primitives::merkle_tree::TxMerkleNodeEncoder;
+    use bitcoin_primitives::transaction::{TxMerkleNodeDecoder, TxMerkleNodeDecoderError};
+}
+
+#[test]
+fn api_can_use_all_types_from_module_transaction() {
+    use bitcoin_primitives::transaction::{
+        OutPointDecoder, OutPointDecoderError, OutPointEncoder, TransactionDecoder,
+        TransactionDecoderError, TransactionEncoder, TxInDecoder, TxInDecoderError, TxInEncoder,
+        TxOutDecoder, TxOutDecoderError, TxOutEncoder, VersionDecoder, VersionDecoderError,
+        VersionEncoder,
+    };
+}
+
+#[test]
+fn api_can_use_all_types_from_module_witness() {
+    use bitcoin_primitives::witness::{WitnessDecoder, WitnessDecoderError, WitnessEncoder};
 }
 
 // `Debug` representation is never empty (C-DEBUG-NONEMPTY).
