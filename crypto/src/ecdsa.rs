f0a0f3308b39a796103f2308067e7b5a68893e5f
Mitchell Bagot <packof14eggies@gmail.com>
2026-05-14 18:27:57 +1000

    Replace usage of secp256k1::Error with new error in ecdsa
    The ecdsa::InvalidDerError can be used both in the key verify
    functions but also to replace the Secp256k1 variant in DecodeError.
    
    Replace DecodeError::Secp256k1 variant with InvalidDer.

--- crypto/src/ecdsa.rs
+++ crypto/src/ecdsa.rs
@@ -60,13 +60,13 @@ impl Signature {
     /// # Errors
     ///
     /// * [`DecodeError::EmptySignature`] if the slice is empty.
-    /// * [`DecodeError::Secp256k1`] if the slice cannot be decoded to an ECDSA signature.
+    /// * [`DecodeError::InvalidDer`] if the slice is not a valid DER encoding for an ECDSA signature.
     pub fn from_slice(sl: &[u8]) -> Result<Self, DecodeError> {
         let (sighash_type, sig) = sl.split_last().ok_or(DecodeError::EmptySignature)?;
         let sighash_type = EcdsaSighashType::from_standard(u32::from(*sighash_type))
             .map_err(DecodeError::SighashType)?;
-        let signature =
-            secp256k1::ecdsa::Signature::from_der(sig).map_err(DecodeError::Secp256k1)?;
+        let signature = secp256k1::ecdsa::Signature::from_der(sig)
+            .map_err(|_| DecodeError::InvalidDer(InvalidDerError))?;
         Ok(Self { signature, sighash_type })
     }
 
@@ -285,8 +285,8 @@ pub mod error {
         SighashType(NonStandardSighashTypeError),
         /// Signature was empty.
         EmptySignature,
-        /// A secp256k1 error.
-        Secp256k1(secp256k1::Error),
+        /// Bad DER encoding for ECDSA signature.
+        InvalidDer(InvalidDerError),
     }
 
     impl From<Infallible> for DecodeError {
@@ -298,7 +298,7 @@ pub mod error {
             match self {
                 Self::SighashType(ref e) => write_err!(f, "non-standard signature hash type"; e),
                 Self::EmptySignature => write!(f, "empty ECDSA signature"),
-                Self::Secp256k1(ref e) => write_err!(f, "secp256k1"; e),
+                Self::InvalidDer(ref e) => write_err!(f, "bad DER encoding for ECDSA signature"; e),
             }
         }
     }
@@ -307,7 +307,7 @@ pub mod error {
     impl std::error::Error for DecodeError {
         fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
             match self {
-                Self::Secp256k1(ref e) => Some(e),
+                Self::InvalidDer(ref e) => Some(e),
                 Self::SighashType(ref e) => Some(e),
                 Self::EmptySignature => None,
             }
