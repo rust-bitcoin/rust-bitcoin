9965a4b2fc85e6b9467a3fcf8d3e61193ead000e
Tobin C. Harding <me@tobin.cc>
2026-05-27 11:20:54 +1000

    Change the code in a symlinked file

--- include/wrap_debug.rs
+++ include/wrap_debug.rs
@@ -2,7 +2,7 @@
 use core::fmt;
 
 /// A wrapper for a function that implements `Debug`.
-pub(crate) struct WrapDebug<F: Fn(&mut fmt::Formatter) -> fmt::Result>(pub F);
+struct WrapDebug<F: Fn(&mut fmt::Formatter) -> fmt::Result>(pub F);
 
 impl<F: Fn(&mut fmt::Formatter) -> fmt::Result> fmt::Debug for WrapDebug<F> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { (self.0)(f) }
