ec1a7f3a6938c333dc85a4646461603ec2a5ed5f
Mitchell Bagot <packof14eggies@gmail.com>
2026-08-05 13:44:29 +1000

    units: Add Div and DivAssign for NonZero to Amount types
    Currently, Amount and SignedAmount can be divided by NonZeroU64/I64,
    and in both cases yield their original types due to the infallibility
    of the operation. In order to mirror Amount ops with NumOpResult and
    *Assign ops, Div<NonZero*> impls should be included for NumOpResult,
    and corresponding DivAssign impls should be added for all types.
    
    Add Div<NonZero*64> for NumOpResult and DivAssign<NonZero*64> for
    Amount, SignedAmount and NumOpResult.

--- units/src/amount/result.rs
+++ units/src/amount/result.rs
@@ -103,6 +103,11 @@ crate::internal_macros::impl_op_for_references! {
 
         fn div(self, rhs: NonZeroU64) -> Self::Output { Self::from_sat(self.to_sat() / rhs.get()).expect("construction after division cannot fail") }
     }
+    impl ops::Div<NonZeroU64> for NumOpResult<Amount> {
+        type Output = NumOpResult<Amount>;
+
+        fn div(self, rhs: NonZeroU64) -> Self::Output { self.map(|lhs| lhs / rhs) }
+    }
     impl ops::Rem<u64> for Amount {
         type Output = NumOpResult<Amount>;
 
@@ -184,6 +189,11 @@ crate::internal_macros::impl_op_for_references! {
 
         fn div(self, rhs: NonZeroI64) -> Self::Output { Self::from_sat(self.to_sat() / rhs.get()).expect("construction after division cannot fail") }
     }
+    impl ops::Div<NonZeroI64> for NumOpResult<SignedAmount> {
+        type Output = NumOpResult<SignedAmount>;
+
+        fn div(self, rhs: NonZeroI64) -> Self::Output { self.map(|lhs| lhs / rhs) }
+    }
     impl ops::Rem<i64> for SignedAmount {
         type Output = NumOpResult<SignedAmount>;
 
@@ -200,6 +210,10 @@ impl_mul_assign!(NumOpResult<Amount>, u64);
 impl_mul_assign!(NumOpResult<SignedAmount>, i64);
 impl_div_assign!(NumOpResult<Amount>, u64);
 impl_div_assign!(NumOpResult<SignedAmount>, i64);
+impl_div_assign!(Amount, NonZeroU64);
+impl_div_assign!(SignedAmount, NonZeroI64);
+impl_div_assign!(NumOpResult<Amount>, NonZeroU64);
+impl_div_assign!(NumOpResult<SignedAmount>, NonZeroI64);
 impl_rem_assign!(NumOpResult<Amount>, u64);
 impl_rem_assign!(NumOpResult<SignedAmount>, i64);
 
