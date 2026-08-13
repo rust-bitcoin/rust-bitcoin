c25ba7a7faf7fd603c352e9ac8aa2198a2500718
jrakibi <j.errakibi@gmail.com>
2026-08-13 22:18:23 +0800

    benches: update to use the new consensus_encoding
    bitcoin::consensus was removed but we forgot to update
    these benchmark to use the new consensus_encoding crate

--- benches/bitcoin/transaction.rs
+++ benches/bitcoin/transaction.rs
@@ -4,9 +4,9 @@ use std::hint::black_box;
 
 use bitcoin::blockdata::transaction::Transaction;
 use bitcoin::blockdata::transaction::TransactionExt as _; // for total_size()
-use bitcoin::consensus::{encode, Encodable};
 use bitcoin::io::sink;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use encoding::{decode_from_hex, decode_from_slice, encode_to_writer};
 
 const SOME_TX: &str = "0100000001a15d57094aa7a21a28cb20b59aab8fc7d1149a3bdbcddba9c622e4f5f6a99ece010000006c493046022100f93bb0e7d8db7bd46e40132d1f8242026e045f03a0efe71bbb8e3f475e970d790221009337cd7f1f929f00cc6ff01f03729b069a7c21b59b1736ddfee5db5946c5da8c0121033b9b137ee87d5a812d6f506efdd37f0affa7ffc310711c06c7f3e097c9447c52ffffffff0100e1f505000000001976a9140389035a9225b3839e2bbf32d826a1e222031fd888ac00000000";
 
@@ -14,26 +14,26 @@ fn bench_tx(c: &mut Criterion) {
     let mut g = c.benchmark_group("transaction");
 
     g.bench_function(BenchmarkId::new("size", "some"), |b| {
-        let mut tx: Transaction = encode::deserialize_hex(SOME_TX).unwrap();
+        let mut tx: Transaction = decode_from_hex(SOME_TX).unwrap();
         b.iter(|| {
             black_box(black_box(&mut tx).total_size());
         });
     });
 
     g.bench_function(BenchmarkId::new("serialize", "some"), |b| {
-        let tx: Transaction = encode::deserialize_hex(SOME_TX).unwrap();
+        let tx: Transaction = decode_from_hex(SOME_TX).unwrap();
         let mut data = Vec::with_capacity(SOME_TX.len() / 2);
         b.iter(|| {
-            let result = tx.consensus_encode(&mut data);
+            let result = encode_to_writer(&tx, &mut data);
             black_box(&result);
             data.clear();
         });
     });
 
     g.bench_function(BenchmarkId::new("serialize_logic", "some"), |b| {
-        let tx: Transaction = encode::deserialize_hex(SOME_TX).unwrap();
+        let tx: Transaction = decode_from_hex(SOME_TX).unwrap();
         b.iter(|| {
-            let size = tx.consensus_encode(&mut sink());
+            let size = encode_to_writer(&tx, sink());
             let _ = black_box(size);
         });
     });
@@ -41,14 +41,14 @@ fn bench_tx(c: &mut Criterion) {
     g.bench_function(BenchmarkId::new("deserialize", "raw_bytes"), |b| {
         let raw_tx = hex::hex!(SOME_TX);
         b.iter(|| {
-            let tx: Transaction = encode::deserialize(&raw_tx).unwrap();
+            let tx: Transaction = decode_from_slice(&raw_tx).unwrap();
             black_box(tx);
         });
     });
 
     g.bench_function(BenchmarkId::new("deserialize_hex", "string"), |b| {
         b.iter(|| {
-            let tx: Transaction = encode::deserialize_hex(SOME_TX).unwrap();
+            let tx: Transaction = decode_from_hex(SOME_TX).unwrap();
             black_box(tx);
         });
     });
