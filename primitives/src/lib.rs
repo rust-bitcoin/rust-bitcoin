54e22747d012e90cc4eaf0d33d0ce9ab40ae6cda
Mitchell Bagot <packof14eggies@gmail.com>
2026-08-13 15:12:38 +1000

    Remove link brackets from deprecation note

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -93,7 +93,7 @@ pub use units::{
     weight::{self, Weight},
 };
 
-#[deprecated(since = "1.0.0-rc.0", note = "use [`BlockHeightInterval`] instead")]
+#[deprecated(since = "1.0.0-rc.0", note = "use `BlockHeightInterval` instead")]
 #[doc(hidden)]
 pub type BlockInterval = BlockHeightInterval;
 
