6767c9b92439a5634d5864dde518db22602a9871
Abeeujah <abeeujah@gmail.com>
2026-08-13 18:22:29 +0100

    bip158: Add BIP-0157 filter hash types
    Implement `FilterHash` and `FilterHeader` wrapper types around double-
    SHA256 hashes as specified in BIP-0157.

--- bip158/src/lib.rs
+++ bip158/src/lib.rs
@@ -1,8 +1,24 @@
 // SPDX-License-Identifier: CC0-1.0
 
-//! # Rust Bitcoin BIP-158 implementation.
+//! # BIP-0158 compact block filters
 
+#![no_std]
 // Coding conventions.
 #![warn(missing_docs)]
 #![warn(deprecated_in_future)]
 #![doc(test(attr(warn(unused))))]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+#[cfg(feature = "std")]
+extern crate std;
+
+mod hash_types;
+
+pub use self::hash_types::{
+    FilterHash, FilterHashDecoder, FilterHashDecoderError, FilterHashEncoder, FilterHeader,
+    FilterHeaderDecoder, FilterHeaderDecoderError, FilterHeaderEncoder,
+};
+
+include!("../include/decoder_newtype.rs"); // decoder_newtype! macro
