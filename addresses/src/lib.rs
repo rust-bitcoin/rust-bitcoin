fe90ee7a2142d6b589604bbc431dbf910675b8d4
Mitchell Bagot <packof14eggies@gmail.com>
2026-08-27 17:50:09 +1000

    addresses: Adjust Address::from_script docs
    In the new crate, the lint requirements for documentation are stricter
    than bitcoin. To satisfy the new requirements, the docs must include
    an Errors section.
    
    Add Errors section to Address::from_script docs and allow the
    missing_panics_doc clippy lint.

--- addresses/src/lib.rs
+++ addresses/src/lib.rs
@@ -880,7 +880,15 @@ impl Address {
     pub fn is_spend_standard(&self) -> bool { self.address_type().is_some() }
 
     /// Constructs a new [`Address`] from an output script (`scriptPubkey`).
+    ///
+    /// # Errors
+    ///
+    /// - [`FromScriptError::UnrecognizedScript`] if the given script is not p2pkh, p2sh, or
+    ///   SegWit program.
+    /// - [`FromScriptError::WitnessProgram`] if the given script has a valid SegWit version, but
+    ///   is not a valid witness program. See [`WitnessProgram::new`] for more details.
     #[cfg(feature = "alloc")]
+    #[allow(clippy::missing_panics_doc)] // Slices have known lengths before array casts
     pub fn from_script(
         script: &ScriptPubKey,
         params: impl Into<AddressParams>,
