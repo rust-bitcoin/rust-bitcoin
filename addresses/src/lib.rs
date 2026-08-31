9974046beb088047e21fd23a5e327297f45a1e7a
Mitchell Bagot <packof14eggies@gmail.com>
2026-08-31 19:01:23 +1000

    addresses: Add network conversion method to Address
    Currently, conversion of the network on an Address requires
    round-tripping via the script and manually reconstructing with
    from_script. This necessitates both alloc, and the full bitcoin crate.
    Instead, we should provide a simple method on the Address that permits
    switching just the network while leaving the payload data unchanged.
    
    Add Address::with_params that constructs a checked Address given a
    new AddressParams.

--- addresses/src/lib.rs
+++ addresses/src/lib.rs
@@ -670,6 +670,43 @@ impl<V: NetworkValidation> Address<V> {
             AddressInner::Segwit { program: _, ref hrp } => (*hrp).to_network_kind(),
         }
     }
+
+    /// Constructs a new address using the given [`AddressParams`].
+    ///
+    /// Only the network of the address is changed, the payload (public key hash, script
+    /// hash, or witness program) is left untouched.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #[cfg(feature = "alloc")]
+    /// # fn example() -> Result<(), bitcoin_addresses::ParseError> {
+    /// use bitcoin_addresses::{Address, AddressParams};
+    ///
+    /// let mainnet: Address = "132F25rTsvBdp9JzLLBHP5mvGY66i1xdiM".parse::<Address<_>>()?
+    ///     .require_network(AddressParams::MAINNET)?;
+    /// assert_eq!(mainnet.to_string(), "132F25rTsvBdp9JzLLBHP5mvGY66i1xdiM");
+    ///
+    /// // The same public key hash, but encoded with the testnet prefix.
+    /// let testnet = mainnet.with_params(AddressParams::TESTNET4);
+    /// assert_eq!(testnet.to_string(), "mhYCK8wSgwctbFnc3u9fCzzF8XgodR9KHX");
+    /// # Ok(())
+    /// # }
+    /// # #[cfg(feature = "alloc")]
+    /// # example().unwrap();
+    /// ```
+    pub fn with_params(&self, params: AddressParams) -> Address {
+        let new_inner = match *self.inner() {
+            AddressInner::P2pkh { hash, network: _ } =>
+                AddressInner::P2pkh { hash, network: params.network_kind },
+            AddressInner::P2sh { hash, network: _ } =>
+                AddressInner::P2sh { hash, network: params.network_kind },
+            AddressInner::Segwit { program, hrp: _ } =>
+                AddressInner::Segwit { program, hrp: params.hrp },
+        };
+        // Fine to return NetworkChecked, as .require_network(network) is infallible.
+        Address::from_inner(new_inner)
+    }
 }
 
 /// Methods and functions that can be called only on `Address<NetworkChecked>`.
